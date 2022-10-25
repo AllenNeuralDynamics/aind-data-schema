032e466c4dfe3524611fac23536bfbb1274d8846
dyf <david.feng@alleninstitute.org>
2022-10-24 21:26:51 -0700

    adding import test

---
+++ tests/test_core.py
@@ -0,0 +1,14 @@
+import unittest
+
+import jsonschema
+import pydantic
+
+import aind_data_schema
+
+class ExampleTest(unittest.TestCase):
+    
+    def test_example(self):
+        assert True
+
+if __name__ == "__main__":
+    unittest.main()
