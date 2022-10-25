d0506bc0787b7e4e1da0cceca017abbecde5b7fe
dyf <david.feng@alleninstitute.org>
2022-10-25 09:37:06 -0700

    WIP for imaging schemas

--- src/aind_data_schema/__init__.py
+++ src/aind_data_schema/__init__.py
@@ -1,3 +1,7 @@
 """Simple package to demo project structure.
 """
-__version__ = "0.0.2"
+__version__ = "0.0.3"
+
+from aind_data_schema.imaging import Acquisition, Instrument
+
+__all__ = [Acquisition,Instrument]
