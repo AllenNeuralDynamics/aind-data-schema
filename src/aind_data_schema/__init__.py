0f30a98c8bf3ab175f4508f335b7ec5e26bd71f3
dyf <david.feng@alleninstitute.org>
2022-10-24 22:41:42 -0700

    adding exports

--- src/aind_data_schema/__init__.py
+++ src/aind_data_schema/__init__.py
@@ -1,3 +1,9 @@
 """Simple package to demo project structure.
 """
 __version__ = "0.0.3"
+
+from .data_description import (
+    DataDescription,
+    RawDataDescription,
+    DerivedDataDescription,
+)
