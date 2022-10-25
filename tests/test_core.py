c4028df1c6457dab3fc8f4ab0860babe834cd594
dyf <david.feng@alleninstitute.org>
2022-10-24 21:37:25 -0700

    linters

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,13 +5,14 @@ import pydantic
 
 import aind_data_schema
 
+
 class ExampleTest(unittest.TestCase):
-    
     def test_example(self):
         print(aind_data_schema)
         print(pydantic)
         print(jsonschema)
         assert True
+
 
 if __name__ == "__main__":
     unittest.main()
