ffec0c75cb911669a847c88e8910be04c5a4385e
dyf <david.feng@alleninstitute.org>
2022-11-01 13:35:51 -0700

    bump

--- src/aind_data_schema/__init__.py
+++ src/aind_data_schema/__init__.py
@@ -1,7 +1,7 @@
 """ imports for BaseModel subclasses
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .data_description import (DataDescription, DerivedDataDescription,
                                RawDataDescription)
