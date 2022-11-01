1cbca1f081f8a876f625a195ad8122b552d4d9da
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2022-11-01 09:15:20 -0700

    feat: version bump to 0.0.13

--- src/aind_data_schema/__init__.py
+++ src/aind_data_schema/__init__.py
@@ -1,6 +1,6 @@
 """ imports for BaseModel subclasses
 """
-__version__ = "0.0.12"
+__version__ = "0.0.13"
 
 from .data_description import (DataDescription, DerivedDataDescription,
                                RawDataDescription)
