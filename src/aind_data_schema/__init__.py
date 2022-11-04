85b45074cf09275c98ae2e4a71eab75fc2a2ac6b
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2022-11-04 13:45:20 -0700

    feat: version bump 0.1.9

--- src/aind_data_schema/__init__.py
+++ src/aind_data_schema/__init__.py
@@ -1,7 +1,7 @@
 """ imports for BaseModel subclasses
 """
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 from .data_description import (DataDescription, DerivedDataDescription,
                                RawDataDescription)
