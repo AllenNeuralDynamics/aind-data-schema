14f576b1cdf27b9cd44a8072a68fa76b3871ef60
Saskia de Vries <sejdevries@gmail.com>
2022-11-08 20:19:52 -0800

    plural

--- src/aind_data_schema/subject.py
+++ src/aind_data_schema/subject.py
@@ -109,5 +109,5 @@ class Subject(BaseModel):
     home_cage_enrichment: Optional[HomeCageEnrichment] = Field(
         None, title="Home cage enrichment"
     )
-    wellness_report: Optional[List[WellnessReport]] = Field(None, title="Wellness Report")
+    wellness_reports: Optional[List[WellnessReport]] = Field(None, title="Wellness Report")
     notes: Optional[str] = Field(None, title="Notes")
