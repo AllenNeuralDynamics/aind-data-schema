007d07573b979b90babec529889a3dc10f47988d
dyf <dyf@fake.email>
2022-11-15 14:02:28 -0800

    lint

--- tests/test_base.py
+++ tests/test_base.py
@@ -4,11 +4,17 @@ from aind_data_schema.base import AindSchema
 
 
 class TestSchema(AindSchema):
+    """a made up schema"""
+
     foo: str
 
 
 class BaseTests(unittest.TestCase):
+    """tests for the base module"""
+
     def test_described_by(self):
+        """test that described by works"""
+
         s = TestSchema(foo="bar")
         self.assertEqual(
             s.describedBy,
