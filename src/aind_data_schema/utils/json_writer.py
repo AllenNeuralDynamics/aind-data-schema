d866c0c93c3807c1287bda5ae7238d6df485b4fa
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2023-01-05 17:39:30 -0800

    docs: adds docstrings

--- src/aind_data_schema/utils/json_writer.py
+++ src/aind_data_schema/utils/json_writer.py
@@ -1,21 +1,23 @@
+""" Utility method to write Pydantic schemas to JSON """
+
 import argparse
-import json
 import os
+import re
 
 import aind_data_schema
 from aind_data_schema.base import AindCoreModel
 
 DEFAULT_FILE_PATH = os.getcwd()
 
+
 class SchemaWriter:
     """ Class to write Pydantic schemas to JSON """
 
-    def __init__(self, args):
-        self.output = args.output
-
     @staticmethod
     def get_schemas():
-
+        """
+        Returns child classes of AindCoreModel in aind_data_schema
+        """
         aind_data_schema_classes = aind_data_schema.__all__
 
         for class_name in aind_data_schema_classes:
@@ -24,31 +26,46 @@ class SchemaWriter:
             if AindCoreModel in model.__bases__:
                 yield model
 
+    def schema_filename(class_name):
+        """ 
+        Returns filename in snakecase
+        """
+        name = class_name.__name__
+        return re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower() + ".json"
 
-    # TO-DO: helper function for filename, fix argparse for output directory
-
-    def write_to_json(self):
+    def write_to_json(self, args):
+        """
+        Writes Pydantic models to JSON file. 
+        Parameters
+        ----------
+        args:
+            optional output dir argument. defaults to current working directory
+        """
         schemas_to_write = SchemaWriter.get_schemas()
 
         for schema in schemas_to_write:
-            s = schema.schema()
-            filename = f"{DEFAULT_FILE_PATH}/{schema.__name__}.json"
+            filename = SchemaWriter.schema_filename(schema)
+            output_file = f"{args.output}/{filename}"
 
-            with open(filename, "w") as f:
+            with open(output_file, "w") as f:
                 f.write(schema.schema_json(indent=3))
 
-def main():
+
+def main(args):
+    """ Writes Pydantic models as JSON """
     s = SchemaWriter.get_schemas()
-    SchemaWriter.write_to_json(s)
+    SchemaWriter.write_to_json(s, args)
+
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument(
-    #     "-o",
-    #     "--output",
-    #     default=DEFAULT_FILE_PATH,
-    #     help="Output directory, defaults to current working directory",
-    # )
-    # args = parser.parse_args()
+    """ User defined argument for output directory """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o",
+        "--output",
+        default=DEFAULT_FILE_PATH,
+        help="Output directory, defaults to current working directory",
+    )
+    args = parser.parse_args()
 
-    main()
+    main(args)
