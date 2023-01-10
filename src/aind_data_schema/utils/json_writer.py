e01c024ce59952a12e4f783769d79b094aac0c5c
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2023-01-09 17:58:41 -0800

    refactor: adds parameter to write_to_json method

--- src/aind_data_schema/utils/json_writer.py
+++ src/aind_data_schema/utils/json_writer.py
@@ -3,7 +3,6 @@
 import argparse
 import os
 import re
-import sys
 
 import aind_data_schema
 from aind_data_schema.base import AindCoreModel
@@ -13,29 +12,6 @@ DEFAULT_FILE_PATH = os.getcwd()
 
 class SchemaWriter:
     """Class to write Pydantic schemas to JSON"""
-
-    def __init__(self, args: list):
-        self.args = args
-
-    def parse_args(self, args: list) -> argparse.Namespace:
-        """Parses sys args with argparse"""
-
-        help_message = (
-            "Output directory, defaults to current working directory"
-        )
-
-        parser = argparse.ArgumentParser()
-
-        parser.add_argument(
-            "-o",
-            "--output",
-            default=DEFAULT_FILE_PATH,
-            help=help_message,
-        )
-
-        optional_args = parser.parse_args()
-
-        return optional_args
 
     @staticmethod
     def get_schemas():
@@ -57,7 +33,7 @@ class SchemaWriter:
         name = class_name.__name__
         return re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower() + ".json"
 
-    def write_to_json(self, args: list):
+    def write_to_json(schemas_to_write, args: list):
         """
         Writes Pydantic models to JSON file.
         Parameters
@@ -65,11 +41,7 @@ class SchemaWriter:
         args:
             optional output directory argument. defaults to current working directory
         """
-        schemas_to_write = SchemaWriter.get_schemas()
-
-        if len(args) > 1:
-            output_path = args[2]
-        output_path = DEFAULT_FILE_PATH
+        output_path = args.output
 
         for schema in schemas_to_write:
             filename = SchemaWriter.schema_filename(schema)
@@ -79,9 +51,23 @@ class SchemaWriter:
                 f.write(schema.schema_json(indent=3))
 
 
-if __name__ == "__main__":
-
+def main(args):
     """Writes Pydantic models as JSON"""
 
     s = SchemaWriter.get_schemas()
-    SchemaWriter.write_to_json(s, sys.argv)
+    SchemaWriter.write_to_json(s, args)
+
+
+if __name__ == "__main__":
+    """User defined argument for output directory"""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o",
+        "--output",
+        default=DEFAULT_FILE_PATH,
+        help="Output directory, defaults to current working directory",
+    )
+    args = parser.parse_args()
+
+    main(args)
--- tests/test_json_writer.py
+++ tests/test_json_writer.py
@@ -1,13 +1,16 @@
 import unittest
 import json
 
+from argparse import Namespace
 from pathlib import Path
 from unittest.mock import patch
 
 from aind_data_schema import DataDescription, Procedures, Processing, Subject
 from aind_data_schema.utils.json_writer import SchemaWriter
 
-TEST_PATH = Path("fake_path") / "aind"
+TEST_ARGS = [ "fake_filename.txt", "--output", "some_test_dir"]
+
+TEST_ARGV = Namespace(TEST_ARGS)
 
 class JsonWriterTests(unittest.TestCase):
     """Tests for the Json Writer module"""
@@ -29,9 +32,12 @@ class JsonWriterTests(unittest.TestCase):
     @patch("builtins.open", new_callable=unittest.mock.mock_open())
     def test_write_to_json(self, mock_json_file):
         """Tests that model is written to JSON"""
-        s = SchemaWriter.write_to_json(Subject, TEST_PATH)
+
+        mock_schemas_to_write = [DataDescription, Procedures, Processing, Subject]
+
+        s = SchemaWriter.write_to_json(mock_schemas_to_write, TEST_ARGS)
         s_filename = SchemaWriter.schema_filename(Subject)
-        expected_path = str(TEST_PATH) + s_filename
+        expected_path =str(TEST_ARGS[2]) + s_filename
 
         mock_json_file.assert_called_once_with(expected_path, "w")
 
