c2148650e3359b820f06a1cc54d1d24496e9b865
dyf <dyf@fake.email>
2023-01-11 14:39:52 -0800

    water restrictions to list

--- src/aind_data_schema/procedures.py
+++ src/aind_data_schema/procedures.py
@@ -287,7 +287,7 @@ class WaterRestriction(AindModel):
 class Procedures(AindCoreModel):
     """Description of all procedures performed on a subject"""
 
-    schema_version: str = Field("0.4.5", description="schema version", title="Version", const=True)
+    schema_version: str = Field("0.5.0", description="schema version", title="Version", const=True)
     subject_id: str = Field(
         ...,
         description="Unique identifier for the subject. If this is not a Allen LAS ID, indicate this in the Notes.",
@@ -308,7 +308,7 @@ class Procedures(AindCoreModel):
         ]
     ] = Field(None, title="Injections", unique_items=True)
     fiber_implants: Optional[List[FiberImplant]] = Field(None, title="Fiber implants", unique_items=True)
-    water_restriction: Optional[WaterRestriction] = Field(None, title="Water restriction")
+    water_restrictions: Optional[List[WaterRestriction]] = Field(None, title="Water restriction")
     training_protocols: Optional[List[TrainingProtocol]] = Field(None, title="Training protocols", unique_items=True)
     tissue_preparations: Optional[List[TissuePrep]] = Field(None, title="Tissue preparations", unique_items=True)
     other_procedures: Optional[List[Procedure]] = Field(None, title="Other procedures", unique_items=True)
