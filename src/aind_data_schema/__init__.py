a6e2549b3e32ca527e9fd578ec81033815c7d52d
Saskia de Vries <sejdevries@gmail.com>
2023-01-12 11:32:21 -0800

    Making craniotomy coordinates optional to allow whole hemisphere craniotomies

--- src/aind_data_schema/__init__.py
+++ src/aind_data_schema/__init__.py
@@ -1,7 +1,7 @@
 """ imports for AindModel subclasses
 """
 
-__version__ = "0.8.0"
+__version__ = "0.8.`"
 
 
 from .data_description import DataDescription, DerivedDataDescription, Funding, RawDataDescription
