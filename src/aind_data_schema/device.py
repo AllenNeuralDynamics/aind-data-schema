79a05124d3e1eaa90de6382380cfa7c7035086c0
Saskia de Vries <sejdevries@gmail.com>
2023-02-10 14:07:57 -0800

    removed laser name from device because redundant

--- src/aind_data_schema/device.py
+++ src/aind_data_schema/device.py
@@ -293,7 +293,6 @@ class Laser(Device):
     """Laser module with a specific wavelength (may be a sub-component of a larger assembly)"""
 
     # required fields
-    name: Optional[str] = Field(None, title="Name")
     manufacturer: Literal[
         Manufacturer.COHERENT_SCIENTIFIC.value,
         Manufacturer.HAMAMATSU.value,
