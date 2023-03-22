d584e918f0dd0c23487dd2f0d13c804dafea08c6
Saskia de Vries <sejdevries@gmail.com>
2023-03-22 13:48:03 -0700

    Adding opto stim to ophys session schema

--- src/aind_data_schema/ophys/ophys_session.py
+++ src/aind_data_schema/ophys/ophys_session.py
@@ -11,6 +11,12 @@ from pydantic import Field
 from ..base import AindCoreModel, AindModel
 from ..device import FrequencyUnit, PowerUnit, SizeUnit
 
+class TimeUnit(Enum):
+    """Time units"""
+
+    S = "seconds"
+    MS = "milliseconds"
+
 
 class FiberName(Enum):
     """Fiber name"""
@@ -82,11 +88,34 @@ class Patch(AindModel):
     output_power_unit: PowerUnit = Field(PowerUnit.UW, title="Output power unit")
 
 
+class OptoStim(AindModel):
+    """Description of optophysiology stimulation parameters"""
+
+    pulse_frequency: int = Field(..., title="Pulse frequency (Hz)")
+    pulse_frequency_unit: FrequencyUnit = Field(FrequencyUnit.HZ, title="Pulse frequency unit")
+    number_pulse_trains: int = Field(..., title="Number of pulse trains")
+    pulse_width: int = Field(..., title="Pulse width (ms)")
+    pulse_width_unit: TimeUnit = Field(TimeUnit.MS, title="Pulse width unit")
+    pulse_train_duration: float = Field(..., title="Pulse train duration (s)")
+    pulse_train_duration_unit: TimeUnit = Field(TimeUnit.S, title="Pulse train duration unit")
+    pulse_train_interval: float = Field(
+        ..., 
+        title="Pulse train interval (s)",
+        description="Time between pulse trains"
+        )
+    pulse_train_interval_unit: TimeUnit = Field(TimeUnit.S, title="Pulse train interval unit")
+    baseline_duration: float = Field(
+        ..., 
+        title="Baseline duration (s)",
+        description="Duration of baseline recording prior to first pulse train")
+    baseline_duration_unit: TimeUnit = Field(TimeUnit.S, title="Baseline duration unit")
+
+
 class OphysSession(AindCoreModel):
     """Description of an ophys session"""
 
     schema_version: str = Field(
-        "0.1.2",
+        "0.1.3",
         description="schema version",
         title="Schema Version",
         const=True,
