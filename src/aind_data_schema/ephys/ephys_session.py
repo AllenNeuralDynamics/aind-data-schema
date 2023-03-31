c500c38784c7ae1f98e07b186624344f09d29af8
Saskia de Vries <sejdevries@gmail.com>
2023-03-31 13:44:58 -0700

    fixing datetime imports

--- src/aind_data_schema/ephys/ephys_session.py
+++ src/aind_data_schema/ephys/ephys_session.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from datetime import datetime
+from datetime import datetime, time
 from enum import Enum
 from typing import List, Optional
 
--- src/aind_data_schema/stimulus.py
+++ src/aind_data_schema/stimulus.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-from datetime import date, time
 from enum import Enum
 from typing import Optional, Dict, Any
 
