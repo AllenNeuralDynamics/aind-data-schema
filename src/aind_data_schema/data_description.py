1ca5ade74698efcde0a6126209e55e22fb26bda0
dyf <dyf@fake.email>
2023-05-05 16:52:23 -0700

    adding merfish/etc

--- src/aind_data_schema/data_description.py
+++ src/aind_data_schema/data_description.py
@@ -79,11 +79,14 @@ class Modality(Enum, metaclass=BaseNameEnumMeta):
     FMOST = BaseName(name="Fluorescence micro-optical sectioning tomography", abbreviation="fMOST")
     HSFP = BaseName(name="Hyperspectral fiber photometry", abbreviation="HSFP")
     ICEPHYS = BaseName(name="Intracellular electrophysiology", abbreviation="icephys")
+    FISH = BaseName(name="Fluorescence in situ Hybridization", abbreviation="fish")
     MESOSPIM = BaseName(name="Mesoscale selective plane illumination microscopy", abbreviation="mesoSPIM")
+    MERFISH = BaseName(name="Multiplexed Error-Robust Fluorescence in situ Hybridization", abbreviation="merfish")
     MRI = BaseName(name="Magnetic resonance imaging", abbreviation="MRI")
     OPHYS = BaseName(name="Optical physiology", abbreviation="ophys")
     SMARTSPIM = BaseName(name="Smart selective plane illumination microscopy", abbreviation="SmartSPIM")
     SPIM = BaseName(name="Selective plane illumination microscopy", abbreviation="SPIM")
+    SPTX = BaseName(name="Spatial transcriptomics", abbreviation="sptx")
 
     @classmethod
     def __modify_schema__(cls, field_schema):
@@ -104,6 +107,7 @@ class ExperimentType(Enum):
     FMOST=Modality.FMOST.value.abbreviation
     HSFP=Modality.HSFP.value.abbreviation
     MESOSPIM=Modality.MESOSPIM.value.abbreviation
+    MERFISH=Modality.MERFISH.value.abbreviation
     MRI=Modality.MRI.value.abbreviation
     SMARTSPIM=Modality.SMARTSPIM.value.abbreviation
     OTHER="Other"
@@ -148,7 +152,7 @@ class RelatedData(AindModel):
 class DataDescription(AindCoreModel):
     """Description of a logical collection of data files"""
 
-    schema_version: str = Field("0.6.0", title="Schema Version", const=True)
+    schema_version: str = Field("0.6.1", title="Schema Version", const=True)
     license: str = Field("CC-BY-4.0", title="License", const=True)
 
     creation_time: time = Field(
