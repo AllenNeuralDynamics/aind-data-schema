59ab74e50bec84b11686bdf36c11dec4f13c9aa3
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2023-05-11 15:54:19 -0400

    Update subject.py

--- examples/subject.py
+++ examples/subject.py
@@ -7,7 +7,7 @@ from aind_data_schema.subject import Housing, Subject, Species
 t = datetime.datetime(2022, 11, 22, 8, 43, 00)
 
 s = Subject(
-    species=Species("Mus musculus"),
+    species="Mus musculus",
     subject_id="12345",
     sex="Male",
     date_of_birth=t.date(),
