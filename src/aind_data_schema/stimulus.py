c2d13d334ae37dd5f4a143366ad48335ead685e6
Saskia de Vries <sejdevries@gmail.com>
2023-05-15 10:26:43 -0700

    missed the title

--- src/aind_data_schema/stimulus.py
+++ src/aind_data_schema/stimulus.py
@@ -83,7 +83,7 @@ class VisualStim(AindModel):
         title="Stimulus script",
         description="The specific code for this stimulus instance",
     )
-    stimulus_script_version: Optional[str] = Field(None, "Stimulus srcipt version")
+    stimulus_script_version: Optional[str] = Field(None, title="Stimulus srcipt version")
     notes: Optional[str] = Field(None, title="Notes")
 
 
