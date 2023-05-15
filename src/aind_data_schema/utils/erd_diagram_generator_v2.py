c7e91bb93a173f94baf988dd7b7362012c6a1d5c
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2023-05-15 18:33:49 -0400

    class-based erd diagram generator

---
+++ src/aind_data_schema/utils/erd_diagram_generator_v2.py
@@ -0,0 +1,46 @@
+from typing import Iterator
+import aind_data_schema
+from aind_data_schema.base import AindCoreModel
+import erdantic as erd
+import os
+
+class ErdDiagramGenerator:
+    """Class to build erdantic diagrams"""
+
+    def __init__(self, classes_to_generate: list) -> None:
+        """Initialize erd diagram generator class"""
+
+        os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+        self.loaded_modules = list(self._get_schemas())
+        
+        if not classes_to_generate: 
+            self.classes_to_generate = list(self.loaded_modules)
+        else:
+            self.classes_to_generate = [module for module in self.loaded_modules if module.__name__ in classes_to_generate]
+
+
+    def generate_aind_core_model_diagrams(self):
+        for module in self.loaded_modules:
+            self.generate_erd_diagram(module)
+
+    def generate_requested_classes(self):
+        for module in self.classes_to_generate:
+            self.generate_erd_diagram(module)
+        
+    def generate_erd_diagram(self, module):
+        diagram = erd.create(module)
+        diagram.draw("ERD_diagrams/" + module.__name__ + '.png')
+
+    @staticmethod
+    def _get_schemas() -> Iterator[AindCoreModel]:
+        """
+        Returns Iterator of AindCoreModel classes
+        """
+        aind_data_schema_classes = aind_data_schema.__all__
+
+        for class_name in aind_data_schema_classes:
+            model = getattr(aind_data_schema, class_name)
+
+            if AindCoreModel in model.__bases__:
+                yield model
