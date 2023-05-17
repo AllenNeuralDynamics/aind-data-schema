7e144432a6b6efd7308bbc60d3ea8e5f60840ceb
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2023-05-17 15:54:42 -0400

    formatting

--- src/aind_data_schema/utils/erd_diagram_generator.py
+++ src/aind_data_schema/utils/erd_diagram_generator.py
@@ -2,6 +2,8 @@ from typing import Iterator
 import aind_data_schema
 from aind_data_schema.base import AindCoreModel
 import erdantic as erd
+import os
+from pathlib import Path
 
 
 class ErdDiagramGenerator:
@@ -17,27 +19,26 @@ class ErdDiagramGenerator:
         # os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
 
         self.loaded_modules = list(self._get_schemas())
-        
-        if not classes_to_generate: # if empty list passed in, generate erd docs for all modules
+
+        if not classes_to_generate:  # if empty list passed in, generate erd docs for all modules
             self.classes_to_generate = self.loaded_modules
         else:
             # List of only models that are present in both loaded_modules and classes_to_generate
-            self.classes_to_generate = [module for module in self.loaded_modules if module.__name__ in classes_to_generate]
-
+            self.classes_to_generate = [
+                module for module in self.loaded_modules if module.__name__ in classes_to_generate
+            ]
 
     def generate_aind_core_model_diagrams(self):
         """generate erd diagrams for all models in loaded models"""
         for module in self.loaded_modules:
             self.generate_erd_diagram(module)
 
-
     def generate_requested_classes(self):
         """generate erd diagrams for all models in classes_to_generate"""
         for module in self.classes_to_generate:
             self.generate_erd_diagram(module)
 
-
-    def generate_erd_diagram(self, module, outpath = "ERD_diagrams/"):
+    def generate_erd_diagram(self, module, outpath: Path = Path("ERD_diagrams")):
         """
         Code to generate a single erd diagram, given a generic class/model
         ie:
@@ -47,9 +48,10 @@ class ErdDiagramGenerator:
 
         Can take output file path as input, otherwise defaults to generic output path
         """
-        diagram = erd.create(module)
-        diagram.draw(outpath + module.__name__ + '.png')
 
+        file_path = outpath / (module.__name__ + ".png")
+        diagram = erd.create(module)
+        diagram.draw(file_path)
 
     @staticmethod
     def _get_schemas() -> Iterator[AindCoreModel]:
