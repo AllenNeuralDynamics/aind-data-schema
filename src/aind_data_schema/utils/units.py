9eb9e15d42569109bebdf211753e7b5f14a23f5d
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2023-05-22 14:22:20 -0400

    Defined multiple generic UnitWithMeasurement classes

--- src/aind_data_schema/utils/units.py
+++ src/aind_data_schema/utils/units.py
@@ -1,68 +1,106 @@
+"""Script for defining UnitWithValue classes"""
+
 from typing import Generic, TypeVar, Optional, List, Type, Union
 from enum import Enum
 
 from pydantic import BaseModel, validator, ValidationError, Field
 from pydantic.generics import GenericModel
 
-ScalarType = TypeVar("ScalarType", bound=Union[int, float])
-UnitType = TypeVar("UnitType")
-
-
-class ValueWithUnit(GenericModel, Generic[ScalarType, UnitType]):
-    value: ScalarType 
-    unit: UnitType
-
 
 class Units:
+    """Class containing enumerations of relevant measurement types"""
 
     class Size(Enum):
-        MM = "mm"
-        CM = "cm"
-        M = "m"
-        KM = "km"
-    
+        """Enumeration of Length Measurements"""
+
+        MM = "millimetre"
+        CM = "centimetre"
+        M = "metre"
+        KM = "kilometre"
     
 
     class Mass(Enum):
-        KG = "kg"
-        G = "g"
-        pass
+        """Enumeration of Mass Measurements"""
+
+        KG = "kilogram"
+        G = "gram"
+        
 
     class Frequency(Enum):
-        pass
+        """Enumeration of Frequency Measurements"""
+
+        mHZ = "millihertz" # UGH this is bad
+        HZ = "hertz"
+        KHZ = "kilohertz"
+        MHZ = "megahertz"
+        GHZ = "gigahertz"
+        THZ = "terahertz"
+
 
     class Volume(Enum):
-        L = "L"
-        ML = "mL"
-        pass
+        """Enumeration of Volume Measurements"""
+
+        L = "litre"
+        ML = "millilitre"
 
 
-    SizeType = TypeVar("SizeType", bound=Size)
-    MassType = TypeVar("MassType", bound=Mass)
-    FrequencyType = TypeVar("FrequencyType", bound=Frequency)
-    VolumeType = TypeVar("VolumeType", bound=Volume)
+    class Angle(Enum):
+        """Enumeration of Angle Measurements"""
+
+        RAD = "radians"
+        DEG = "degrees"
 
 
-    # SizeValue = ValueWithUnit[ScalarType,SizeType]
-    MassValue = ValueWithUnit[ScalarType,MassType]
-    FrequencyValue = ValueWithUnit[ScalarType,FrequencyType]
-    VolumeValue = ValueWithUnit[ScalarType,VolumeType]
+ScalarType = TypeVar("ScalarType", float, int)
 
 
-# ScalarType = TypeVar('ScalarType')
-# UnitType = TypeVar('UnitType', bound=str)
+class GenericValues:
+    """Constructs UnitWithValue class for each relevant measurement type"""
 
-# class ScalarValue(GenericModel, Generic[ScalarType, UnitType]):
-#     value: ScalarType
-#     unit: UnitType
+    SizeType = TypeVar("SizeType", bound=Units.Size)
+    MassType = TypeVar("MassType", bound=Units.Mass)
+    FrequencyType = TypeVar("FrequencyType", bound=Units.Frequency)
+    VolumeType = TypeVar("VolumeType", bound=Units.Volume)
+    AngleType = TypeVar("AngleType", bound=Units.Angle)
 
 
-# class SizeUnit(Enum):
-#     MM = "mm"
-#     KM = "km"
-    
-# SizeValue = ScalarValue[float, SizeUnit]
+    class SizeValue(GenericModel, Generic[ScalarType, SizeType]):
+        """Generic for Size Measurements"""
 
-class SizeValue(GenericModel, Generic[ScalarType,UnitType]):
         value: ScalarType
-        unit: UnitType = Units.Size.CM
+        unit: Units.SizeType = Units.Size.M
+
+
+    class MassValue(GenericModel, Generic[ScalarType, MassType]):
+        """Generic for Mass Measurements"""
+
+        value: ScalarType
+        unit: Units.MassType = Units.Mass.G
+
+
+    class VolumeValue(GenericModel, Generic[ScalarType, VolumeType]):
+        """Generic for Volume Measurements"""
+        
+        value: ScalarType
+        unit: Units.VolumeType = Units.Volume.L
+
+
+    class FrequencyValue(GenericModel, Generic[ScalarType, FrequencyType]):
+        """Generic for Frequency Measurements"""
+        
+        value: ScalarType
+        unit: Units.FrequencyType = Units.Frequency.HZ
+
+
+    class AngleValue(GenericModel, Generic[ScalarType, AngleType]):
+        """Generic for Angle Measurements"""
+        
+        value: ScalarType
+        unit: Units.AngleType = Units.Angle.RAD
+
+
+SizeVal = GenericValues.SizeValue[ScalarType, Units.SizeType]
+MassVal = GenericValues.MassValue[ScalarType, Units.MassType]
+VolumeVal = GenericValues.VolumeValue[ScalarType, Units.VolumeType]
+FrequencyVal = GenericValues.FrequencyValue[ScalarType, Units.FrequencyType]
+AngleVal = GenericValues.AngleValue[ScalarType, Units.AngleType]
