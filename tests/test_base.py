ca221050ea84a96eafda223b699d65abd5a3cb3d
Louise-X10 <77957732+Louise-X10@users.noreply.github.com>
2023-06-14 15:19:33 -0700

    refactor: rename mock variable

--- tests/test_base.py
+++ tests/test_base.py
@@ -51,18 +51,18 @@ class BaseTests(unittest.TestCase):
         )
 
     @patch("builtins.open", new_callable=unittest.mock.mock_open())
-    def test_write_standard_file_no_prefix(self, mocked_file):
+    def test_write_standard_file_no_prefix(self, mock_open):
         """tests that standard file is named and written as expected with no prefix"""
         p = Procedures.construct()
         default_filename = p.default_filename()
         json_contents = p.json(indent=3)
         p.write_standard_file()
 
-        mocked_file.assert_called_once_with(default_filename, "w")
-        mocked_file.return_value.__enter__().write.assert_called_once_with(json_contents)
+        mock_open.assert_called_once_with(default_filename, "w")
+        mock_open.return_value.__enter__().write.assert_called_once_with(json_contents)
 
     @patch("builtins.open", new_callable=unittest.mock.mock_open())
-    def test_write_standard_file_with_file_prefix(self, mocked_file):
+    def test_write_standard_file_with_file_prefix(self, mock_open):
         """tests that standard file is named and written as expected with path to file prefix"""
         p = Procedures.construct()
         default_filename = p.default_filename()
@@ -74,8 +74,8 @@ class BaseTests(unittest.TestCase):
         # foo/bar/aibs_procedure.json
         expected_file_path = str(new_path) + "_" + default_filename
 
-        mocked_file.assert_called_once_with(expected_file_path, "w")
-        mocked_file.return_value.__enter__().write.assert_called_once_with(json_contents)
+        mock_open.assert_called_once_with(expected_file_path, "w")
+        mock_open.return_value.__enter__().write.assert_called_once_with(json_contents)
 
     @patch("builtins.open", new_callable=unittest.mock.mock_open())
     def test_write_standard_file_with_directory_prefix(self, mock_open):
