65b746270c5702cf24949a4c28990ec1a38a979f
Saskia de Vries <sejdevries@gmail.com>
2023-06-23 17:02:54 -0700

    version bump again

--- src/aind_data_schema/ophys/ophys_rig.py
+++ src/aind_data_schema/ophys/ophys_rig.py
@@ -80,7 +80,7 @@ class OphysRig(AindCoreModel):
     """Description of an optical physiology rig"""
 
     schema_version: str = Field(
-        "0.5.3",
+        "0.5.4",
         description="schema version",
         title="Schema Version",
         const=True,
