d3dba6cfecc480c2f4a847f009277b2d76b73a08
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2023-06-27 15:05:52 -0400

    Update device.py

--- src/aind_data_schema/device.py
+++ src/aind_data_schema/device.py
@@ -381,11 +381,11 @@ class Camera(Device):
     # required fields
     data_interface: DataInterface = Field(..., title="Type of connection to PC")
     manufacturer: Literal[
-        Manufacturer.ALLIED.name,
-        Manufacturer.BASLER.name,
-        Manufacturer.EDMUND_OPTICS.name,
-        Manufacturer.FLIR.name,
-        Manufacturer.THORLABS.name,
+        Manufacturer.ALLIED,
+        Manufacturer.BASLER,
+        Manufacturer.EDMUND_OPTICS,
+        Manufacturer.FLIR,
+        Manufacturer.THORLABS,
         Manufacturer.OTHER,
     ]
     computer_name: str = Field(..., title="Name of computer receiving data from this camera")
@@ -408,7 +408,7 @@ class Lens(Device):
     """Lens used to focus light onto a camera sensor"""
 
     # required fields
-    manufacturer: Literal[Manufacturer.EDMUND_OPTICS.name, Manufacturer.THORLABS.name, Manufacturer.OTHER]
+    manufacturer: Literal[Manufacturer.EDMUND_OPTICS, Manufacturer.THORLABS, Manufacturer.OTHER]
 
     # optional fields
     focal_length: Optional[float] = Field(None, title="Focal length of the lens", units="mm")
@@ -426,10 +426,10 @@ class Filter(Device):
     # required fields
     filter_type: FilterType = Field(..., title="Type of filter")
     manufacturer: Literal[
-        Manufacturer.EDMUND_OPTICS.name,
-        Manufacturer.CHROMA.name,
-        Manufacturer.SEMROCK.name,
-        Manufacturer.THORLABS.name,
+        Manufacturer.EDMUND_OPTICS,
+        Manufacturer.CHROMA,
+        Manufacturer.SEMROCK,
+        Manufacturer.THORLABS,
         Manufacturer.OTHER,
     ]
 
@@ -518,9 +518,9 @@ class DAQDevice(Device):
     # required fields
     data_interface: DataInterface = Field(..., title="Type of connection to PC")
     manufacturer: Literal[
-        Manufacturer.NATIONAL_INSTRUMENTS.name,
-        Manufacturer.IMEC.name,
-        Manufacturer.OEPS.name,
+        Manufacturer.NATIONAL_INSTRUMENTS,
+        Manufacturer.IMEC,
+        Manufacturer.OEPS,
         Manufacturer.OTHER,
     ]
     computer_name: str = Field(..., title="Name of computer controlling this DAQ")
@@ -558,10 +558,10 @@ class Laser(Device):
     # required fields
     lightsource_type: str = Field("Laser", title="Lightsource type")
     manufacturer: Literal[
-        Manufacturer.COHERENT_SCIENTIFIC.name,
-        Manufacturer.HAMAMATSU.name,
-        Manufacturer.OXXIUS.name,
-        Manufacturer.QUANTIFI.name,
+        Manufacturer.COHERENT_SCIENTIFIC,
+        Manufacturer.HAMAMATSU,
+        Manufacturer.OXXIUS,
+        Manufacturer.QUANTIFI,
         Manufacturer.OTHER,
     ]
     wavelength: int = Field(..., title="Wavelength (nm)", units="nm")
@@ -589,9 +589,9 @@ class LightEmittingDiode(Device):
 
     lightsource_type: str = Field("LED", title="Lightsource type")
     manufacturer: Literal[
-        Manufacturer.DORIC.name,
-        Manufacturer.PRIZMATIX.name,
-        Manufacturer.THORLABS.name,
+        Manufacturer.DORIC,
+        Manufacturer.PRIZMATIX,
+        Manufacturer.THORLABS,
         Manufacturer.OTHER,
     ]
     wavelength: int = Field(..., title="Wavelength (nm)", units="nm")
@@ -639,7 +639,7 @@ class Monitor(Device):
     """Visual display"""
 
     # required fields
-    manufacturer: Literal[Manufacturer.LG.name]
+    manufacturer: Literal[Manufacturer.LG]
     refresh_rate: int = Field(..., title="Refresh rate (Hz)", units="Hz", ge=60)
     width: int = Field(..., title="Width (pixels)", units="pixels")
     height: int = Field(..., title="Height (pixels)", units="pixels")
