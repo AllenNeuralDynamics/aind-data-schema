44c7371e7d7a13ca1b42968db8c9407d13763464
Saskia de Vries <sejdevries@gmail.com>
2023-07-22 21:18:39 -0700

    update more examples

--- src/aind_data_schema/ephys/ephys_session.py
+++ src/aind_data_schema/ephys/ephys_session.py
@@ -125,7 +125,7 @@ class Stream(AindModel):
 class EphysSession(AindCoreModel):
     """Description of an ephys recording session"""
 
-    schema_version: str = Field("0.4.5", description="schema version", title="Version", const=True)
+    schema_version: str = Field("0.4.6", description="schema version", title="Version", const=True)
     experimenter_full_name: List[str] = Field(
         ...,
         description="First and last name of the experimenter(s).",
