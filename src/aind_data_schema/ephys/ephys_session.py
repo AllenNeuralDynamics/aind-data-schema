f53ef5da97d34b23d96dd2168533e87b6680eef6
dyf <david.feng@alleninstitute.org>
2023-08-09 06:25:24 -0700

    missed one

--- src/aind_data_schema/ephys/ephys_session.py
+++ src/aind_data_schema/ephys/ephys_session.py
@@ -128,7 +128,7 @@ class Stream(AindModel):
 class EphysSession(AindCoreModel):
     """Description of an ephys recording session"""
 
-    schema_version: str = Field("0.4.7", description="schema version", title="Version", const=True)
+    schema_version: str = Field("0.4.8", description="schema version", title="Version", const=True)
     experimenter_full_name: List[str] = Field(
         ...,
         description="First and last name of the experimenter(s).",
