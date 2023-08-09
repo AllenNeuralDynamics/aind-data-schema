651f8d247d18db51ce9d4478c7bf3edaba5c494a
dyf <david.feng@alleninstitute.org>
2023-08-09 06:13:16 -0700

    schema version bumps

--- src/aind_data_schema/ophys/ophys_session.py
+++ src/aind_data_schema/ophys/ophys_session.py
@@ -103,7 +103,7 @@ class OphysSession(AindCoreModel):
     """Description of an ophys session"""
 
     schema_version: str = Field(
-        "0.2.5",
+        "0.2.6",
         description="schema version",
         title="Schema Version",
         const=True,
