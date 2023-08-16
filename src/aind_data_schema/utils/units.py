4ec5c9fc7d91b0ff254423970019b4dceaa9af68
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2023-08-16 16:28:32 -0400

    Removing outdated implementation

--- src/aind_data_schema/utils/units.py
+++ src/aind_data_schema/utils/units.py
@@ -96,37 +96,37 @@ size_2d_fields = {'width', 'height'}
 
 filter_size_fields = {'diameter', 'width', 'height'}
 
-def create_unit_with_value(model_name, field_names_and_data_types, scalar_type, unit_type, unit_default):
+def create_unit_with_value(model_name, field_names, scalar_type, unit_type, unit_default):
     """this uses create_model instead of generics, which lets us set default values"""
     if scalar_type:
-        m = create_model(model_name, **{k: (scalar_type, ...) for k in field_names_and_data_types}, unit=(unit_type, unit_default))
+        m = create_model(model_name, **{k: (scalar_type, ...) for k in field_names}, unit=(unit_type, unit_default))
     return m
 
 
-Coord3DValue = create_unit_with_value("test_unit", {'x', 'y', 'z'}, SizeUnit, SizeUnit.MM, Decimal)
+# Coord3DValue = create_unit_with_value("test_unit", {'x', 'y', 'z'}, SizeUnit, SizeUnit.MM, Decimal)
 
-SizeValueMM = create_unit_with_value("SizeValueMM", Decimal, SizeUnit, SizeUnit.MM)
-SizeValueCM = create_unit_with_value("SizeValueCM", Decimal, SizeUnit, SizeUnit.CM)
-SizeValuePX = create_unit_with_value("SizeValuePX", int, SizeUnit, SizeUnit.PX)
-SizeValueIN = create_unit_with_value("SizeValueIN", Decimal, SizeUnit, SizeUnit.IN)
-SizeValueNM = create_unit_with_value("SizeValueNM", Decimal, SizeUnit, SizeUnit.NM)
+# SizeValueMM = create_unit_with_value("SizeValueMM", Decimal, SizeUnit, SizeUnit.MM)
+# SizeValueCM = create_unit_with_value("SizeValueCM", Decimal, SizeUnit, SizeUnit.CM)
+# SizeValuePX = create_unit_with_value("SizeValuePX", int, SizeUnit, SizeUnit.PX)
+# SizeValueIN = create_unit_with_value("SizeValueIN", Decimal, SizeUnit, SizeUnit.IN)
+# SizeValueNM = create_unit_with_value("SizeValueNM", Decimal, SizeUnit, SizeUnit.NM)
 
-FilterSizeValue = create_unit_with_value("FilterSizeValue", filter_size_fields, SizeUnit, SizeUnit.MM)
+# FilterSizeValue = create_unit_with_value("FilterSizeValue", filter_size_fields, SizeUnit, SizeUnit.MM)
 
-WaveLengthNM = create_unit_with_value("WaveLengthNM", int, SizeUnit, SizeUnit.NM)
-MassValue = create_unit_with_value("MassValue", Decimal, MassUnit, MassUnit.MG)
-VolumeValue = create_unit_with_value("VolumeValue", Decimal, VolumeUnit, VolumeUnit.NL)
-FrequencyValueHZ = create_unit_with_value("FrequencyValue", Decimal, FrequencyUnit, FrequencyUnit.HZ)
-AngleValue = create_unit_with_value("AngleValue", Decimal, AngleUnit, AngleUnit.DEG)
-TimeValue = create_unit_with_value("TimeValue", Decimal, TimeUnit, TimeUnit.S)
-PowerValue = create_unit_with_value("PowerValue", Decimal, PowerUnit, PowerUnit.MW)
+# WaveLengthNM = create_unit_with_value("WaveLengthNM", int, SizeUnit, SizeUnit.NM)
+# MassValue = create_unit_with_value("MassValue", Decimal, MassUnit, MassUnit.MG)
+# VolumeValue = create_unit_with_value("VolumeValue", Decimal, VolumeUnit, VolumeUnit.NL)
+# FrequencyValueHZ = create_unit_with_value("FrequencyValue", Decimal, FrequencyUnit, FrequencyUnit.HZ)
+# AngleValue = create_unit_with_value("AngleValue", Decimal, AngleUnit, AngleUnit.DEG)
+# TimeValue = create_unit_with_value("TimeValue", Decimal, TimeUnit, TimeUnit.S)
+# PowerValue = create_unit_with_value("PowerValue", Decimal, PowerUnit, PowerUnit.MW)
 
-CoordValue3D = create_unit_with_value("CoordValue3D", coord_3d_fields, SizeUnit, SizeUnit.MM)
-CoordValue2D = create_unit_with_value("CoordValue2D", coord_2d_fields, SizeUnit, SizeUnit.MM)
+# CoordValue3D = create_unit_with_value("CoordValue3D", coord_3d_fields, SizeUnit, SizeUnit.MM)
+# CoordValue2D = create_unit_with_value("CoordValue2D", coord_2d_fields, SizeUnit, SizeUnit.MM)
 
-SizeValue2DPX = create_unit_with_value("SizeValue2DPX", size_2d_fields, SizeUnit, SizeUnit.PX, int)
-SizeValue3DMM = create_unit_with_value("SizeValue3DMM", size_3d_fields, SizeUnit, SizeUnit.MM)
+# SizeValue2DPX = create_unit_with_value("SizeValue2DPX", size_2d_fields, SizeUnit, SizeUnit.PX, int)
+# SizeValue3DMM = create_unit_with_value("SizeValue3DMM", size_3d_fields, SizeUnit, SizeUnit.MM)
 
-OrientationValue3D = create_unit_with_value("OrientationValue3D", orientation_3d_fields, AngleUnit, AngleUnit.DEG)
-ModuleOrientationValue2D = create_unit_with_value("ModuleOrientationValue2D", module_orientation_2d_fields, AngleUnit, AngleUnit.DEG)
-ModuleOrientationValue3D = create_unit_with_value("ModuleOrientationValue3D", module_orientation_3d_fields, AngleUnit, AngleUnit.DEG)
+# OrientationValue3D = create_unit_with_value("OrientationValue3D", orientation_3d_fields, AngleUnit, AngleUnit.DEG)
+# ModuleOrientationValue2D = create_unit_with_value("ModuleOrientationValue2D", module_orientation_2d_fields, AngleUnit, AngleUnit.DEG)
+# ModuleOrientationValue3D = create_unit_with_value("ModuleOrientationValue3D", module_orientation_3d_fields, AngleUnit, AngleUnit.DEG)
