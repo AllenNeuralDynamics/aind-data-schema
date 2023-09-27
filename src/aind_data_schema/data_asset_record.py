23e0dcae7431f686661c8a2c729f7b1d52c65d70
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2023-09-27 11:51:15 -0700

    fixes and linters

--- src/aind_data_schema/data_asset_record.py
+++ src/aind_data_schema/data_asset_record.py
@@ -1,15 +1,27 @@
 """Module for models"""
 
 from datetime import datetime
+from enum import Enum
 
-from pydantic import BaseModel, Extra, Field
+from pydantic import Extra, Field
+
+from aind_data_schema.base import AindCoreModel
 
 
-class DataAssetRecord(BaseModel):
+class MetadataStatus(Enum):
+    """Status of Metadata"""
+
+    VALID = "Valid"
+    INVALID = "Invalid"
+    MISSING = "Missing"
+    UNKNOWN = "Unknown"
+
+
+class DataAssetRecord(AindCoreModel):
     """The records in the Data Asset Collection needs to contain certain fields
     to easily query and index the data."""
 
-    code_ocean_id: str = Field(
+    id: str = Field(
         ...,
         alias="_id",
         title="Data Asset ID",
@@ -17,44 +29,26 @@ class DataAssetRecord(BaseModel):
     )
     name: str = Field(
         ...,
-        alias="_name",
         description="Name of the data asset.",
         title="Data Asset Name",
     )
     created: datetime = Field(
         ...,
-        alias="_created",
         title="Created",
         description="The data and time the data asset created.",
     )
+    last_modified: datetime = Field(
+        ..., title="Last Modified", description="The date and time that the data asset was last modified."
+    )
     location: str = Field(
         ...,
-        alias="_location",
         title="Location",
         description="Current location of the data asset.",
     )
+    metadata_status: MetadataStatus = Field(..., title=" Metadata Status", description="The status of the metadata.")
+    schema_version: str = Field("0.0.1", title="Schema Version", const=True)
 
     class Config:
         """Need to allow for additional fields to append to base model"""
 
         extra = Extra.allow
-
-    @property
-    def _id(self):
-        """Property for _id"""
-        return self.code_ocean_id
-
-    @property
-    def _name(self):
-        """Property for _name"""
-        return self.name
-
-    @property
-    def _location(self):
-        """Property for _location"""
-        return self.location
-
-    @property
-    def _created(self):
-        """Property for _created"""
-        return self.created
---
+++ tests/test_data_asset_record.py
@@ -0,0 +1,30 @@
+import unittest
+from datetime import datetime
+
+from aind_data_schema.data_asset_record import DataAssetRecord, MetadataStatus
+from aind_data_schema.subject import Subject
+
+
+class TestDataAssetRecord(unittest.TestCase):
+    """Class to test DataAssetRecord model"""
+
+    def test_constructors(self):
+        d1 = DataAssetRecord(
+            _id="00000",
+            name="Test Name",
+            created=datetime(2023, 9, 27, 0, 0, 0),
+            last_modified=datetime(2023, 9, 28, 10, 20, 30),
+            location="Test Location",
+            metadata_status=MetadataStatus.VALID,
+        )
+        assert d1 is not None
+        d2 = DataAssetRecord(
+            _id="00000",
+            name="Test Name",
+            created=datetime(2023, 9, 27, 0, 0, 0),
+            last_modified=datetime(2023, 9, 28, 10, 20, 30),
+            location="Test Location",
+            metadata_status=MetadataStatus.VALID,
+            subject=Subject.construct(),
+        )
+        assert d2 is not None
