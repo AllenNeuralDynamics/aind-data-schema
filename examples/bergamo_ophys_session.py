ef7a4f4e04255605a5a7f2608a07a3f709deda2b
Saskia de Vries <sejdevries@gmail.com>
2023-10-10 23:10:57 -0700

    bergamo example

--- examples/bergamo_ophys_session.py
+++ examples/bergamo_ophys_session.py
@@ -2,13 +2,20 @@
 
 import datetime
 
-from aind_data_schema.ophys.ophys_session import Camera, Detector, FieldOfView, Laser, TwoPhotonOphysSession
+from aind_data_schema.data_description import Modality
+from aind_data_schema.session import (
+    Detector,
+    FieldOfView,
+    Laser,
+    Session,
+    Stream
+)
 from aind_data_schema.stimulus import PhotoStimulation, PhotoStimulationGroup, StimulusEpoch
 
 t = datetime.datetime(2022, 7, 12, 7, 00, 00)
 t2 = datetime.time(7, 00, 00)
 
-s = TwoPhotonOphysSession(
+s = Session(
     experimenter_full_name=["John Doe"],
     session_start_time=t,
     session_end_time=t,
@@ -16,23 +23,45 @@ s = TwoPhotonOphysSession(
     session_type="BCI Photometry",
     iacuc_protocol="2115",
     rig_id="ophys_rig",
-    light_sources=[
-        Laser(
-            name="Laser A",
-            wavelength=405,
-            wavelength_unit="nanometer",
-            excitation_power=10,
-            excitation_power_unit="milliwatt",
+    data_streams=[
+        Stream(
+            stream_start_time=t,
+            stream_end_time=t,
+            stream_modalities=[Modality.POPHYS],
+            light_sources=[
+                Laser(
+                    name="Laser A",
+                    wavelength=405,
+                    wavelength_unit="nanometer",
+                    excitation_power=10,
+                    excitation_power_unit="milliwatt",
+                ),
+            ],
+            detectors=[
+                Detector(
+                    name="PMT A",
+                    exposure_time=0.1,
+                    trigger_type="Internal",
+                ),
+            ],
+            camera_names=["Face Camera"],
+            ophys_fovs=[
+                FieldOfView(
+                    index=0,
+                    imaging_depth=150,
+                    targeted_structure="M1",
+                    fov_coordinate_ml=1.5,
+                    fov_coordinate_ap=1.5,
+                    fov_reference="Bregma",
+                    fov_width=800,
+                    fov_height=800,
+                    magnification="1x",
+                    fov_scale_factor=1.5,
+                    frame_rate=20,
+                ),
+            ],
         ),
     ],
-    detectors=[
-        Detector(
-            name="PMT A",
-            exposure_time=0.1,
-            trigger_type="Internal",
-        ),
-    ],
-    cameras=[Camera(name="Face Camera")],
     stimulus_epochs=[
         StimulusEpoch(
             stimulus=PhotoStimulation(
@@ -64,21 +93,6 @@ s = TwoPhotonOphysSession(
             stimulus_end_time=t2,
         )
     ],
-    fovs=[
-        FieldOfView(
-            index=0,
-            imaging_depth=150,
-            targeted_structure="M1",
-            fov_coordinate_ml=1.5,
-            fov_coordinate_ap=1.5,
-            fov_reference="Bregma",
-            fov_width=800,
-            fov_height=800,
-            magnification="1x",
-            fov_scale_factor=1.5,
-            frame_rate=20,
-        ),
-    ],
 )
 
-s.write_standard_file(prefix="bergamo")
+s.write_standard_file(prefix="bergamo_ophys")
