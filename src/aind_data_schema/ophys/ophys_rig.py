f17b3096266e88bb5ca162094ca8d562c0010129
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2023-10-11 15:32:11 -0700

    schema upgrade (after merging main)

--- src/aind_data_schema/ophys/ophys_rig.py
+++ src/aind_data_schema/ophys/ophys_rig.py
@@ -89,7 +89,7 @@ class OphysRig(AindCoreModel):
     """Description of an optical physiology rig. This is being deprecated after 2023-11-01."""
 
     schema_version: str = Field(
-        "0.7.2",
+        "0.7.3",
         description="schema version",
         title="Schema Version",
         const=True,
