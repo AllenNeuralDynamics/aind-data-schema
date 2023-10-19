6ba53b2bb9d64cf69c4e21dded1d8fd716c4c838
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2023-10-19 16:50:49 -0700

    SmartSPIM and Ecephys metadata

--- src/aind_data_schema/metadata.py
+++ src/aind_data_schema/metadata.py
@@ -3,10 +3,15 @@
 from datetime import datetime
 from enum import Enum
 
-from pydantic import Extra, Field
+from pydantic import Extra, Field, root_validator
 
 from aind_data_schema.base import AindCoreModel
 from aind_data_schema.subject import Subject
+from aind_data_schema.data_description import DataDescription
+from aind_data_schema.procedures import Procedures
+from aind_data_schema.rig import Rig
+from aind_data_schema.session import Session
+from aind_data_schema.processing import Processing
 
 
 class MetadataStatus(Enum):
@@ -58,3 +63,73 @@ class Metadata(AindCoreModel):
         """Need to allow for additional fields to append to base model"""
 
         extra = Extra.allow
+
+
+class SmartSPIMMetadata(Metadata):
+    """The smartSPIM records in the Data Asset Collection require certain metadata."""
+    data_description: DataDescription = Field(
+        ...,
+        title="Data Description",
+        description="Description of a logical collection of data files."
+    )
+    procedures: Procedures = Field(
+        ...,
+        title="Procedures",
+        description="Description of all procedures performed on a subject."
+    )
+    session: Session = Field(
+        ...,
+        title="Session",
+        description="Description of a session."
+    )
+    rig: Rig = Field(
+        ...,
+        title="Rig",
+        description="Description of a rig."
+    )
+    processing: Processing = Field(
+        ...,
+        title="Processing",
+        description="Description of all processes run on data."
+    )
+
+    @root_validator
+    def validate_institute_subject(cls, v):
+        """Validator for Institute mice"""
+        subject_id_value = v.get("subject_id")
+        subject_value = v.get('subject')
+        procedures_value = v.get('procedures')
+        if 'AK' in subject_id_value:
+            if subject_value is None or procedures_value is None:
+                raise ValueError("Both subject and procedures are required "
+                                 "when the metadata is describing Allen Institute mice.")
+        return v
+
+
+class EcephysMetadata(Metadata):
+    """The ecephys records in the Data Asset Collection require certain metadata."""
+    data_description: DataDescription = Field(
+        ...,
+        title="Data Description",
+        description="Description of a logical collection of data files"
+    )
+    procedures: Procedures = Field(
+        ...,
+        title="Procedures",
+        description="Description of all procedures performed on a subject"
+    )
+    session: Session = Field(
+        ...,
+        title="Session",
+        description="Description of a session."
+    )
+    rig: Rig = Field(
+        ...,
+        title="Rig",
+        description="Description of a rig."
+    )
+    processing: Processing = Field(
+        ...,
+        title="Processing",
+        description="Description of all processes run on data."
+    )
