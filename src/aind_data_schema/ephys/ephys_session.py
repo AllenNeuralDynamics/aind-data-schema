f7d3a7b2310d47695bc2d4be72b2c1b50bb09310
Saskia de Vries <sejdevries@gmail.com>
2023-11-02 09:44:50 -0700

    whitespace

--- src/aind_data_schema/ephys/ephys_session.py
+++ src/aind_data_schema/ephys/ephys_session.py
@@ -126,7 +126,7 @@ class Stream(AindModel):
 
 
 class EphysSession(AindCoreModel):
-    """Description of an ephys recording session. This is being deprecated after 2023-11-01. 
+    """Description of an ephys recording session. This is being deprecated after 2023-11-01.
     Use Session class instead."""
 
     schema_version: str = Field("0.4.13", description="schema version", title="Version", const=True)
