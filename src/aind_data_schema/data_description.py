0163a20ce4446855e3d2e88c24f3165f3de1f9b7
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2023-11-10 17:28:40 -0500

    schema vers

--- src/aind_data_schema/data_description.py
+++ src/aind_data_schema/data_description.py
@@ -203,7 +203,7 @@ class RelatedData(AindModel):
 class DataDescription(AindCoreModel):
     """Description of a logical collection of data files"""
 
-    schema_version: str = Field("0.10.2", title="Schema Version", const=True)
+    schema_version: str = Field("0.11.0", title="Schema Version", const=True)
     license: str = Field("CC-BY-4.0", title="License", const=True)
 
     creation_time: datetime = Field(
