8c5596c659c9aecf05b9f08e5dfa2b26ba45c072
Saskia de Vries <sejdevries@gmail.com>
2023-12-14 20:52:38 -0800

    make device name required

--- src/aind_data_schema/core/rig.py
+++ src/aind_data_schema/core/rig.py
@@ -37,7 +37,7 @@ class Rig(AindCoreModel):
 
     _DESCRIBED_BY_URL = AindCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/rig.py"
     describedBy: str = Field(_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
-    schema_version: Literal["0.2.0"] = Field("0.2.0")
+    schema_version: Literal["0.2.1"] = Field("0.2.1")
 
     rig_id: str = Field(..., description="room_stim apparatus_version", title="Rig ID")
     modification_date: date = Field(..., title="Date of modification")
