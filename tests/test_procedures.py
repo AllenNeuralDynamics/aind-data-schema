67f8dec6906515aa1f60322cca22016d7d92c780
Saskia de Vries <sejdevries@gmail.com>
2023-12-18 21:13:49 -0800

    linting

--- tests/test_procedures.py
+++ tests/test_procedures.py
@@ -34,7 +34,6 @@ class ProceduresTests(unittest.TestCase):
         """Tests validation on the presence of injections materials"""
 
         start_date = date.fromisoformat("2020-10-10")
-        end_date = date.fromisoformat("2020-10-11")
 
         with self.assertRaises(ValidationError):
             Procedures(
@@ -80,54 +79,54 @@ class ProceduresTests(unittest.TestCase):
                                 injection_duration=1,
                                 recovery_time=10,
                             ),
-                        NanojectInjection(
-                            protocol_id="bca",
-                            injection_materials=[
-                                InjectionMaterial(
-                                    name="abc",
-                                    titer="5.46E13",
-                                    prep_lot_number="CT323",
-                                )
-                            ],
-                            injection_duration=1,
-                            injection_coordinate_ml=1,
-                            injection_coordinate_ap=1,
-                            injection_coordinate_depth=[1],
-                            injection_coordinate_reference="Bregma",
-                            bregma_to_lambda_distance=4.1,
-                            injection_angle=1,
-                            injection_volume=[1],
-                            recovery_time=10,
-                            targeted_structure="VISp6",
-                        ),
-                        FiberImplant(
-                            protocol_id="dx.doi.org/120.123/fkjd",
-                            probes=[
-                                OphysProbe(
-                                    ophys_probe=FiberProbe(
-                                        device_type="Fiber optic probe",
-                                        name="Probe A",
-                                        manufacturer=Manufacturer.DORIC,
-                                        model="8",
-                                        core_diameter=2,
-                                        numerical_aperture=1,
-                                        ferrule_material="Ceramic",
-                                        total_length=10,
-                                    ),
-                                    targeted_structure="MOp",
-                                    stereotactic_coordinate_ap=1,
-                                    stereotactic_coordinate_dv=2,
-                                    stereotactic_coordinate_ml=3,
-                                    stereotactic_coordinate_reference="Bregma",
-                                    bregma_to_lambda_distance=4.1,
-                                    angle=10,
-                                )
-                            ],
-                        ),
-                    ]
-                )
-            ],
-        )
+                            NanojectInjection(
+                                protocol_id="bca",
+                                injection_materials=[
+                                    InjectionMaterial(
+                                        name="abc",
+                                        titer="5.46E13",
+                                        prep_lot_number="CT323",
+                                    )
+                                ],
+                                injection_duration=1,
+                                injection_coordinate_ml=1,
+                                injection_coordinate_ap=1,
+                                injection_coordinate_depth=[1],
+                                injection_coordinate_reference="Bregma",
+                                bregma_to_lambda_distance=4.1,
+                                injection_angle=1,
+                                injection_volume=[1],
+                                recovery_time=10,
+                                targeted_structure="VISp6",
+                            ),
+                            FiberImplant(
+                                protocol_id="dx.doi.org/120.123/fkjd",
+                                probes=[
+                                    OphysProbe(
+                                        ophys_probe=FiberProbe(
+                                            device_type="Fiber optic probe",
+                                            name="Probe A",
+                                            manufacturer=Manufacturer.DORIC,
+                                            model="8",
+                                            core_diameter=2,
+                                            numerical_aperture=1,
+                                            ferrule_material="Ceramic",
+                                            total_length=10,
+                                        ),
+                                        targeted_structure="MOp",
+                                        stereotactic_coordinate_ap=1,
+                                        stereotactic_coordinate_dv=2,
+                                        stereotactic_coordinate_ml=3,
+                                        stereotactic_coordinate_reference="Bregma",
+                                        bregma_to_lambda_distance=4.1,
+                                        angle=10,
+                                    )
+                                ],
+                            ),
+                        ]
+                    )
+                ],
+            )
 
         self.assertEqual(3, len(p.subject_procedures))
 
