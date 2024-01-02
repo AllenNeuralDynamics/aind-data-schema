9885eb98d2d9f78a0cdc64505e9480c8cd23996f
Saskia de Vries <sejdevries@gmail.com>
2024-01-02 11:21:18 -0800

    version bumps

--- src/aind_data_schema/core/procedures.py
+++ src/aind_data_schema/core/procedures.py
@@ -518,7 +518,7 @@ class Procedures(AindCoreModel):
     _DESCRIBED_BY_URL = AindCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/procedures.py"
     describedBy: str = Field(_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
 
-    schema_version: Literal["0.10.3"] = Field("0.10.3", description="schema version", title="Version")
+    schema_version: Literal["0.10.4"] = Field("0.10.4", description="schema version", title="Version")
     subject_id: str = Field(
         ...,
         description="Unique identifier for the subject. If this is not a Allen LAS ID, indicate this in the Notes.",
