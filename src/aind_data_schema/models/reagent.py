98364124acc059bcceb47c4ec4d65dd2af2dde5d
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-01-08 18:06:33 -0500

    str to Institution

--- src/aind_data_schema/models/reagent.py
+++ src/aind_data_schema/models/reagent.py
@@ -7,13 +7,14 @@ from pydantic import Field
 
 from aind_data_schema.base import AindModel
 from aind_data_schema.models.pid_names import PIDName
+from aind_data_schema.models.institutions import Institution
 
 
 class Reagent(AindModel):
     """Description of reagent used in procedure"""
 
     name: str = Field(..., title="Name")
-    source: str = Field(..., title="Source")
+    source: Institution = Field(..., title="Source")
     rrid: Optional[PIDName] = Field(None, title="Research Resource ID")
     lot_number: str = Field(..., title="Lot number")
     expiration_date: Optional[date] = Field(None, title="Lot expiration date")
