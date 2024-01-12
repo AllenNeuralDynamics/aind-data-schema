308307bdcc7d10f8e78a1b382d154297b5fc3fa4
Saskia de Vries <sejdevries@gmail.com>
2024-01-12 15:35:12 -0800

    port index string

--- src/aind_data_schema/models/devices.py
+++ src/aind_data_schema/models/devices.py
@@ -262,7 +262,7 @@ class Device(AindModel):
     manufacturer: Optional[Manufacturer.ONE_OF] = Field(None, title="Manufacturer")
     model: Optional[str] = Field(None, title="Model")
     path_to_cad: Optional[str] = Field(None, title="Path to CAD diagram", description="For CUSTOM manufactured devices")
-    port_index: Optional[int] = Field(None, title="Port index")
+    port_index: Optional[str] = Field(None, title="Port index")
     notes: Optional[str] = Field(None, title="Notes")
 
 
