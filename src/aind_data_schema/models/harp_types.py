b08ac59010553723028d481ed816aa51c6bb82b3
Saskia de Vries <sejdevries@gmail.com>
2024-01-12 16:07:43 -0800

    linting

--- src/aind_data_schema/models/harp_types.py
+++ src/aind_data_schema/models/harp_types.py
@@ -5,7 +5,8 @@ from typing import Literal, Union
 from pydantic import ConfigDict, Field
 from typing_extensions import Annotated
 
-class _HarpDeviceType(PIDName):
+
+class _HarpDeviceType(AindModel):
     """Base model config"""
 
     model_config = ConfigDict(frozen=True)
@@ -14,70 +15,70 @@ class _HarpDeviceType(PIDName):
 class Behavior(_HarpDeviceType):
     """Behavior"""
 
-    name = "Behavior"
+    name = Literal["Behavior"] = "Behavior"
     whoami = 1216
 
 
 class CameraController(_HarpDeviceType):
     """Camera Controller"""
 
-    name = "Camera Controller"
+    name = Literal["Camera Controller"] = "Camera Controller"
     whoami = 1168
 
 
 class ClockSynchronizer(_HarpDeviceType):
     """Clock Synchronizer"""
 
-    name = "Clock Synchronizer"
+    name = Literal["Clock Synchronizer"] = "Clock Synchronizer"
     whoami = 1152
 
 
 class InputExpander(_HarpDeviceType):
     """Input Expander"""
 
-    name = "Input Expander"
+    name = Literal["Input Expander"] = "Input Expander"
     whoami = 1106
 
 
 class LoadCells(_HarpDeviceType):
     """Load Cells"""
 
-    name = "Load Cells"
+    name = Literal["Load Cells"] = "Load Cells"
     whoami = 1232
 
 
 class Olfactometer(_HarpDeviceType):
     """Olfactometer"""
 
-    name = "Olfactometer"
+    name = Literal["Olfactometer"] = "Olfactometer"
     whoami = 1140
 
 
 class SoundCard(_HarpDeviceType):
     """Sound Card"""
 
-    name = "Sound Card"
+    name = Literal["Sound Card"] = "Sound Card"
     whoami = 1280
 
 
 class Synchronizer(_HarpDeviceType):
     """Synchronizer"""
 
-    name = "Synchronizer"
+    name = Literal["Synchronizer"] = "Synchronizer"
     whoami = 1104
 
 
 class TimestampGeneratorGen1(_HarpDeviceType):
     """Timestamp Generator Gen 1"""
 
-    name = "Timestamp Generator Gen 1"
+    name = Literal["Timestamp Generator Gen 1"] = "Timestamp Generator Gen 1"
     whoami = 1154
 
 
 class TimestampGeneratorGen3(_HarpDeviceType):
     """Timestamp Generator Gen 3"""
 
-    name = "Timestamp Generator Gen 3"
+    name = Literal["Timestamp Generator Gen 3"] = "Timestamp Generator Gen 3"
     whoami = 1158
 
 
@@ -97,4 +98,3 @@ class HarpDeviceType:
 
     _ALL = tuple(_HarpDeviceType.__subclasses__())
     ONE_OF = Annotated[Union[_ALL], Field(discriminator="name")]
-    
