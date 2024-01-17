a3030a96ae8dc085a0dd11fbfba237064f6412cb
Saskia de Vries <sejdevries@gmail.com>
2024-01-17 13:58:12 -0800

    fix syntax

--- src/aind_data_schema/models/harp_types.py
+++ src/aind_data_schema/models/harp_types.py
@@ -1,7 +1,6 @@
 """Module for Harp Device Types"""
 
 from typing import Literal, Union
-from enum import Enum
 
 from pydantic import BaseModel, ConfigDict, Field
 from typing_extensions import Annotated
@@ -18,71 +17,71 @@ class _HarpDeviceType(BaseModel):
 class Behavior(_HarpDeviceType):
     """Behavior"""
 
-    name = Literal["Behavior"] = "Behavior"
-    whoami = Literal[1216] = 1216
+    name: Literal["Behavior"] = "Behavior"
+    whoami: Literal[1216] = 1216
 
 
 class CameraController(_HarpDeviceType):
     """Camera Controller"""
 
-    name = Literal["Camera Controller"] = "Camera Controller"
-    whoami = Literal[1168] = 1168
+    name: Literal["Camera Controller"] = "Camera Controller"
+    whoami: Literal[1168] = 1168
 
 
 class ClockSynchronizer(_HarpDeviceType):
     """Clock Synchronizer"""
 
-    name = Literal["Clock Synchronizer"] = "Clock Synchronizer"
-    whoami = Literal[1152] = 1152
+    name: Literal["Clock Synchronizer"] = "Clock Synchronizer"
+    whoami: Literal[1152] = 1152
 
 
 class InputExpander(_HarpDeviceType):
     """Input Expander"""
 
-    name = Literal["Input Expander"] = "Input Expander"
-    whoami = Literal[1106] = 1106
+    name: Literal["Input Expander"] = "Input Expander"
+    whoami: Literal[1106] = 1106
 
 
 class LoadCells(_HarpDeviceType):
     """Load Cells"""
 
-    name = Literal["Load Cells"] = "Load Cells"
-    whoami = Literal[1232] = 1232
+    name: Literal["Load Cells"] = "Load Cells"
+    whoami: Literal[1232] = 1232
 
 
 class Olfactometer(_HarpDeviceType):
     """Olfactometer"""
 
-    name = Literal["Olfactometer"] = "Olfactometer"
-    whoami = Literal[1140] = 1140
+    name: Literal["Olfactometer"] = "Olfactometer"
+    whoami: Literal[1140] = 1140
 
 
 class SoundCard(_HarpDeviceType):
     """Sound Card"""
 
-    name = Literal["Sound Card"] = "Sound Card"
-    whoami = Literal[1280] = 1280
+    name: Literal["Sound Card"] = "Sound Card"
+    whoami: Literal[1280] = 1280
 
 
 class Synchronizer(_HarpDeviceType):
     """Synchronizer"""
 
-    name = Literal["Synchronizer"] = "Synchronizer"
-    whoami = Literal[1104] = 1104
+    name: Literal["Synchronizer"] = "Synchronizer"
+    whoami: Literal[1104] = 1104
 
 
 class TimestampGeneratorGen1(_HarpDeviceType):
     """Timestamp Generator Gen 1"""
 
-    name = Literal["Timestamp Generator Gen 1"] = "Timestamp Generator Gen 1"
-    whoami = Literal[1154] = 1154
+    name: Literal["Timestamp Generator Gen 1"] = "Timestamp Generator Gen 1"
+    whoami: Literal[1154] = 1154
 
 
 class TimestampGeneratorGen3(_HarpDeviceType):
     """Timestamp Generator Gen 3"""
 
-    name = Literal["Timestamp Generator Gen 3"] = "Timestamp Generator Gen 3"
-    whoami = Literal[1158] = 1158
+    name: Literal["Timestamp Generator Gen 3"] = "Timestamp Generator Gen 3"
+    whoami: Literal[1158] = 1158
 
 
 class HarpDeviceType:
