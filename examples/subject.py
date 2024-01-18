9751e84419393edb0ad08fa28a39bdb8fbd8bc7b
Saskia de Vries <sejdevries@gmail.com>
2024-01-17 21:03:56 -0800

    updated example

--- examples/subject.py
+++ examples/subject.py
@@ -14,8 +14,14 @@ s = Subject(
     sex=Sex.MALE,
     date_of_birth=t.date(),
     source=Institution.AI,
-    breeding_info=BreedingInfo(),
-    genotype="Emx1-IRES-Cre;Camk2a-tTA;Ai93(TITL-GCaMP6f)",
+    breeding_info=BreedingInfo(
+        breeding_group= "Emx1-IRES-Cre(ND)",
+        maternal_id="546543",
+        maternal_genotype="Emx1-IRES-Cre/wt; Camk2a-tTa/Camk2a-tTA",
+        paternal_id="232323",
+        paternal_genotype="Ai93(TITL-GCaMP6f)/wt", 
+    ),
+    genotype="Emx1-IRES-Cre/wt;Camk2a-tTA/wt;Ai93(TITL-GCaMP6f)/wt",
     housing=Housing(home_cage_enrichment=["Running wheel"], cage_id="123"),
     background_strain="C57BL/6J",
 )
