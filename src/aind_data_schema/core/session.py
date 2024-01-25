b409587d65f63a9c4484c396df9e727bd60a7adf
Micah Woodard <micah.woodard@alleninstitute.org>
2024-01-25 09:33:46 -0800

    feat:fixed typo and updated examples

--- src/aind_data_schema/core/session.py
+++ src/aind_data_schema/core/session.py
@@ -340,7 +340,7 @@ class Session(AindCoreModel):
     _DESCRIBED_BY_URL = AindCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/session.py"
     describedBy: str = Field(_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
     schema_version: Literal["0.1.3"] = Field("0.1.3")
-    List[str] = Field([], title="Protocol ID", description="DOI for protocols.io")
+    protocol_id: List[str] = Field([], title="Protocol ID", description="DOI for protocols.io")
     experimenter_full_name: List[str] = Field(
         ...,
         description="First and last name of the experimenter(s).",
