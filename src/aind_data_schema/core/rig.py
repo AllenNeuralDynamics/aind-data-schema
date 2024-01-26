ba80198e915519948709efd0eab039a556064a70
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2024-01-25 18:00:05 -0800

    fix: updates schema versions

--- src/aind_data_schema/core/rig.py
+++ src/aind_data_schema/core/rig.py
@@ -49,7 +49,7 @@ class Rig(AindCoreModel):
 
     _DESCRIBED_BY_URL = AindCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/rig.py"
     describedBy: str = Field(_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
-    schema_version: Literal["0.2.7"] = Field("0.2.7")
+    schema_version: Literal["0.2.6"] = Field("0.2.6")
     rig_id: str = Field(..., description="room_stim apparatus_version", title="Rig ID")
     modification_date: date = Field(..., title="Date of modification")
     mouse_platform: MOUSE_PLATFORMS
