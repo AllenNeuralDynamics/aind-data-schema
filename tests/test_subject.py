54a9999de789444c2389971db40cefd99640ef64
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-01-26 14:26:18 -0500

    Update test_subject.py

--- tests/test_subject.py
+++ tests/test_subject.py
@@ -11,7 +11,6 @@ from aind_data_schema.models.species import Species
 
 from aind_data_schema.models.pid_names import PIDName
 from aind_data_schema.models.registry import Registry
-from aind_data_schema.models.species import Species
 
 
 class SubjectTests(unittest.TestCase):
