358259ea8a3ac9e1ab324f1b046726ab7f2656b3
Saskia de Vries <sejdevries@gmail.com>
2024-02-07 21:22:35 -0800

    version bumps

--- src/aind_data_schema/core/session.py
+++ src/aind_data_schema/core/session.py
@@ -339,7 +339,7 @@ class Session(AindCoreModel):
 
     _DESCRIBED_BY_URL = AindCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/session.py"
     describedBy: str = Field(_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
-    schema_version: Literal["0.1.4"] = Field("0.1.4")
+    schema_version: Literal["0.1.6"] = Field("0.1.6")
     protocol_id: List[str] = Field([], title="Protocol ID", description="DOI for protocols.io")
     experimenter_full_name: List[str] = Field(
         ...,
