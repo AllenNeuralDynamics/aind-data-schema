010e4e14aa1cd43523c1cbfa3470919c81058344
Jon Young <104453205+jtyoung84@users.noreply.github.com>
2024-02-22 22:12:37 -0800

    feat: adds module to handle schema versions

--- src/aind_data_schema/utils/schema_version_bump.py
+++ src/aind_data_schema/utils/schema_version_bump.py
@@ -1,11 +1,9 @@
-"""Utility script for bumping schema versions"""
+"""Module to handle the schema_versions"""
 
-import argparse
 import json
 import os
-import sys
 from pathlib import Path
-from typing import Optional, List, Dict, Type
+from typing import Dict, List
 
 import dictdiffer
 import semver
@@ -22,11 +20,7 @@ CORE_SCHEMA_DIR = ROOT_DIR / "src"
 class SchemaVersionHandler:
     """Class that manages semantic versioning of the schemas"""
 
-    def __init__(
-            self,
-            commit_message: str = '',
-            json_schemas_location: Path = Path(OLD_SCHEMA_DIR)
-    ):
+    def __init__(self, commit_message: str = "", json_schemas_location: Path = Path(OLD_SCHEMA_DIR)):
         """
         Class constructor
         Parameters
@@ -40,15 +34,20 @@ class SchemaVersionHandler:
         self.commit_message = commit_message
         self.json_schemas_location = json_schemas_location
 
-    def get_list_of_models_that_changed(self) -> List[AindCoreModel]:
-        """Get a list of core models that have been updated."""
+    def _get_list_of_models_that_changed(self) -> List[AindCoreModel]:
+        """
+        Get a list of core models that have been updated by comparing the json
+        schema of the models to the json schema in the schemas folder.
+        Returns
+        -------
+        List[AindCoreModel]
+          A list of AindCoreModels that changed.
+        """
         schemas_that_need_updating = []
         for core_model in SchemaWriter.get_schemas():
             core_model_json = core_model.model_json_schema()
             default_filename = core_model.default_filename()
-            main_branch_schema_path = (
-                    self.json_schemas_location / default_filename
-            )
+            main_branch_schema_path = self.json_schemas_location / default_filename
             with open(main_branch_schema_path, "r") as f:
                 main_branch_schema_contents = json.load(f)
             diff = dictdiffer.diff(main_branch_schema_contents, core_model_json)
@@ -57,7 +56,20 @@ class SchemaVersionHandler:
         return schemas_that_need_updating
 
     @staticmethod
-    def get_incremented_versions_map(models_that_changed: List[AindCoreModel]) -> Dict[AindCoreModel, str]:
+    def _get_incremented_versions_map(models_that_changed: List[AindCoreModel]) -> Dict[AindCoreModel, str]:
+        """
+
+        Parameters
+        ----------
+        models_that_changed : List[AindCoreModel]
+          A list of models that have been updated and need to have their version numbers incremented.
+
+        Returns
+        -------
+        Dict[AindCoreModel, str]
+          A mapping of the AindCoreModel to its new version number.
+
+        """
         version_bump_map = {}
         # TODO: Use commit message to determine version number to bump?
         for model in models_that_changed:
@@ -67,7 +79,21 @@ class SchemaVersionHandler:
         return version_bump_map
 
     @staticmethod
-    def get_updated_file(python_file_path, new_ver: str) -> list:
+    def _get_updated_file(python_file_path: str, new_ver: str) -> list:
+        """
+        Will read the lines of the file at python_file_path and replaces
+        the line with the old version number with the updated version.
+        Parameters
+        ----------
+        python_file_path : str
+          Path of the python file that will be updated
+        new_ver : str
+          The new version number
+
+        Returns
+        -------
+
+        """
         new_file_contents = []
         with open(python_file_path, "rb") as f:
             file_lines = f.readlines()
@@ -81,116 +107,61 @@ class SchemaVersionHandler:
         return new_file_contents
 
     @staticmethod
-    def write_new_file(new_file_contents: list, python_file_path) -> None:
-        with open(python_file_path, 'wb') as f:
+    def _write_new_file(new_file_contents: list, python_file_path: str) -> None:
+        """
+        Abstracting the write operation to its own method in case we want a
+        different file writer.
+        Parameters
+        ----------
+        new_file_contents : list
+          A list of encoded strings
+        python_file_path : str
+          The Path of the python to overwrite
+
+        Returns
+        -------
+        None
+          Writes the contents to the file path
+
+        """
+        with open(python_file_path, "wb") as f:
             for line in new_file_contents:
                 f.write(line)
 
-    def update_files(self, version_bump_map: Dict[AindCoreModel, str]) -> None:
+    def _update_files(self, version_bump_map: Dict[AindCoreModel, str]) -> None:
+        """
+        Using the information in the version_bump_map, will update the python
+        files in the core directory.
+        Parameters
+        ----------
+        version_bump_map : Dict[AindCoreModel, str]
+          The models that need updating are in the dictionary keys and the
+          new version number is the dictionary value.
+
+        Returns
+        -------
+        None
+          Updates the files in the core directory.
+
+        """
         for model, new_ver in version_bump_map.items():
-            python_file_path = os.path.join(CORE_SCHEMA_DIR,
-                                            *model.__module__.split(
-                                                ".")) + ".py"
-            new_file_contents = self.get_updated_file(python_file_path, new_ver)
-            self.write_new_file(new_file_contents, python_file_path)
+            python_file_path = os.path.join(CORE_SCHEMA_DIR, *model.__module__.split(".")) + ".py"
+            new_file_contents = self._get_updated_file(python_file_path, new_ver)
+            self._write_new_file(new_file_contents, python_file_path)
 
     def run_job(self):
-        list_of_models_that_changed = self.get_list_of_models_that_changed()
-        list_of_inceremented_versions = self.get_incremented_versions_map(list_of_models_that_changed)
-        self.update_files(list_of_inceremented_versions)
-
-
-
-
-
-def bump_version(old_ver: Optional[str]) -> str:
-    """
-    Bumps an old version to a new version .
-    Parameters
-    ----------
-
-    old_ver : str
-      Like "0.1.3"
-
-    Returns
-    -------
-    str
-
-    """
-
-    try:
-        old_v = semver.Version.parse(old_ver)
-    except (TypeError, ValueError):
-        return f"Malformed version: {old_ver}"
-
-    new_v = old_v.bump_patch()
-    new_ver = str(new_v)
-
-    return new_ver
-
-
-def run_job(new_schema_folder: str) -> None:
-    """
-    Loops through files in folders and creates a list of common files.
-    For each common file, if they are equal, nothing happens. If they are
-    different, the old schema version will be bumped and replaced with a
-    new version in the corresponding core model.
-    ----------
-    new_schema_folder : str
-
-    Returns
-    -------
-    None
-
-    """
-
-    def open_json_file(path):
-        """Helper method to read a json file"""
-        with open(path, "r") as f:
-            data = json.load(f)
-        return data
-
-    files_in_old_schema_folder = os.listdir(OLD_SCHEMA_DIR)
-    files_in_new_schema_folder = os.listdir(Path(new_schema_folder))
-    common_files: list = list(set(files_in_old_schema_folder).intersection(set(files_in_new_schema_folder)))
-    for file_name in common_files:
-        old_model = open_json_file(OLD_SCHEMA_DIR / file_name)
-        new_model = open_json_file(Path(new_schema_folder) / file_name)
-        diff = dictdiffer.diff(old_model, new_model)
-        is_equal = len(list(diff)) == 0
-        if not is_equal:
-            old_schema_version_dict = old_model["properties"].get("schema_version")
-            old_schema_version = (
-                None
-                if old_schema_version_dict is None or not isinstance(old_schema_version_dict, dict)
-                else old_schema_version_dict.get("const")
-            )
-            new_schema_version = bump_version(old_schema_version)
-            core_schema_file = CORE_SCHEMA_DIR / file_name.replace("_schema.json", ".py")
-
-            with open(core_schema_file, 'r') as file:
-                for line in file:
-                    if line.lstrip().startswith("schema"):
-                        current_version_line = line
-            with open(core_schema_file, 'r') as file:
-                filedata = file.read()
-            bumped_version_line = current_version_line.replace(old_schema_version, new_schema_version)
-            filedata = filedata.replace(current_version_line, bumped_version_line)
-            with open(core_schema_file, 'w') as file:
-                file.write(filedata)
-            print(f"Schema version in {file_name.replace('_schema.json', '')} have been bumped to {new_schema_version}")
-    return None
+        """
+        This method will compare the json schema generated by the core models to the json schema
+        in the schemas folder, see which schemas have changed, increment the versions, and update
+        the necessary files.
+        """
+        list_of_models_that_changed = self._get_list_of_models_that_changed()
+        list_of_incremented_versions = self._get_incremented_versions_map(list_of_models_that_changed)
+        self._update_files(list_of_incremented_versions)
 
 
 if __name__ == "__main__":
-    sys_args = sys.argv[1:]
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-n",
-        "--new-schema-folder",
-        required=True,
-    )
-    folder_args = parser.parse_args(sys_args)
-    run_job(
-        new_schema_folder=folder_args.new_schema_folder,
-    )
+
+    # TODO: Pass in the commit message as an argument
+    schema_version_handler = SchemaVersionHandler()
+    schema_version_handler.run_job()
