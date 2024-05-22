cb073e8b563479b7f03a7cd8bee6becd640d976f
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-05-22 15:44:00 -0400

    Convert all `device_type`s to sentence casing (#936)
    * all devices to sentence casing
    
    * updating examples
    
    * Update session.py
    
    * Update devices.py
    
    * Update session.py

--- src/aind_data_schema/core/session.py
+++ src/aind_data_schema/core/session.py
@@ -82,7 +82,7 @@ class DetectorConfig(AindModel):
 class LightEmittingDiodeConfig(AindModel):
     """Description of LED settings"""
 
-    device_type: Literal["LightEmittingDiode"] = "LightEmittingDiode"
+    device_type: Literal["Light emitting diode"] = "Light emitting diode"
     name: str = Field(..., title="Name")
     excitation_power: Optional[Decimal] = Field(None, title="Excitation power (mW)")
     excitation_power_unit: PowerUnit = Field(PowerUnit.MW, title="Excitation power unit")
@@ -112,8 +112,9 @@ class FieldOfView(AindModel):
     frame_rate: Optional[Decimal] = Field(None, title="Frame rate (Hz)")
     frame_rate_unit: FrequencyUnit = Field(FrequencyUnit.HZ, title="Frame rate unit")
     coupled_fov_index: Optional[int] = Field(None, title="Coupled FOV", description="Coupled planes for multiscope")
-    power: Optional[Decimal] = Field(None, title="Power",
-                                     description="For coupled planes, this power is shared by both planes")
+    power: Optional[Decimal] = Field(
+        None, title="Power", description="For coupled planes, this power is shared by both planes"
+    )
     power_unit: PowerUnit = Field(PowerUnit.PERCENT, title="Power unit")
     power_ratio: Optional[Decimal] = Field(None, title="Power ratio for coupled planes")
     scanfield_z: Optional[int] = Field(
