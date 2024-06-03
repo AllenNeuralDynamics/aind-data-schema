9bee501e22f83c6253a8189a2d16469326252a5d
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-06-03 18:16:30 -0400

    patching out write calls

--- tests/test_examples_generator.py
+++ tests/test_examples_generator.py
@@ -9,15 +9,20 @@ from aind_data_schema.utils.examples_generator import ExamplesGenerator
 class ExamplesGeneratorTest(unittest.TestCase):
     """Tests for ExamplesGenerator methods"""
 
-    @patch("AindCoreModel.write_standard_file()")
-    def test_generate_all_files(self):
+    @patch("aind_data_schema.base.AindCoreModel.write_standard_file")
+    def test_generate_all_files(self, mocked_run_path):
         """Test generate_all_examples method"""
 
         ExamplesGenerator().generate_all_examples()
 
-    def test_generate_example(self):
+    @patch("aind_data_schema.base.AindCoreModel.write_standard_file")
+    def test_generate_example(self, mocked_run_path):
         """Test generate_example method"""
 
         ExamplesGenerator().generate_example(
             "C:/Users/mae.moninghoff/Documents/GitHub/aind-data-schema/examples/subject.py"
+        )
+
+        ExamplesGenerator().generate_example(
+            "fake/path/to/example.py"
         )
