0d7b6141b52417a8f1a917f1edf08db59ddbb509
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-06-03 17:51:07 -0400

    linters

--- src/aind_data_schema/utils/examples_generator.py
+++ src/aind_data_schema/utils/examples_generator.py
@@ -9,22 +9,28 @@ CURRENT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 ROOT_DIR = CURRENT_DIR.parents[2]
 EXAMPLES_DIR = ROOT_DIR / "examples"
 
-class ExamplesGenerator():
-    def __init__(self):
-        pass
+
+class ExamplesGenerator:
+    """Class to generate example files from .py files in examples directory"""
 
     def generate_all_examples(self):
+        """Generate all examples in EXAMPLES_DIR"""
+
         print(f"Running all examples in {EXAMPLES_DIR}")
         for example_file in glob(f"{EXAMPLES_DIR}/*.py"):
             print(f"Running {example_file}")
             runpy.run_path(path_name=example_file)
-    
+
     def generate_example(self, example_file):
+        """Generate example from example_file"""
+
         print(f"Running {example_file}")
         try:
             runpy.run_path(path_name=example_file)
         except Exception as e:
             print(f"Error running {example_file}: {e}")
 
+
 if __name__ == "__main__":
+    """Run all examples in EXAMPLES_DIR"""
     ExamplesGenerator().generate_all_examples()
--- tests/test_examples_generator.py
+++ tests/test_examples_generator.py
@@ -1,15 +1,23 @@
 """Module to test the examples_generator.py script"""
 
-import json
-import os
 import unittest
-from pathlib import Path
-from unittest.mock import MagicMock, call, mock_open, patch
+from unittest.mock import patch
+
 from aind_data_schema.utils.examples_generator import ExamplesGenerator
+
 
 class ExamplesGeneratorTest(unittest.TestCase):
     """Tests for ExamplesGenerator methods"""
 
     @patch("AindCoreModel.write_standard_file()")
     def test_generate_all_files(self):
+        """Test generate_all_examples method"""
+
         ExamplesGenerator().generate_all_examples()
+
+    def test_generate_example(self):
+        """Test generate_example method"""
+
+        ExamplesGenerator().generate_example(
+            "C:/Users/mae.moninghoff/Documents/GitHub/aind-data-schema/examples/subject.py"
+        )
