88a0d71440b8fd09d8aba02879b4ff71686d5acc
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-06-03 17:41:46 -0400

    class-based implementation

--- src/aind_data_schema/utils/examples_generator.py
+++ src/aind_data_schema/utils/examples_generator.py
@@ -9,8 +9,22 @@ CURRENT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 ROOT_DIR = CURRENT_DIR.parents[2]
 EXAMPLES_DIR = ROOT_DIR / "examples"
 
-if __name__ == "__main__":
-    print(f"Running all examples in {EXAMPLES_DIR}")
-    for example_file in glob(f"{EXAMPLES_DIR}/*.py"):
+class ExamplesGenerator():
+    def __init__(self):
+        pass
+
+    def generate_all_examples(self):
+        print(f"Running all examples in {EXAMPLES_DIR}")
+        for example_file in glob(f"{EXAMPLES_DIR}/*.py"):
+            print(f"Running {example_file}")
+            runpy.run_path(path_name=example_file)
+    
+    def generate_example(self, example_file):
         print(f"Running {example_file}")
-        runpy.run_path(path_name=example_file)
+        try:
+            runpy.run_path(path_name=example_file)
+        except Exception as e:
+            print(f"Error running {example_file}: {e}")
+
+if __name__ == "__main__":
+    ExamplesGenerator().generate_all_examples()
