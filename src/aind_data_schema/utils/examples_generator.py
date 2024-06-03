c2dae3b45d8f8d07382b15f581fed3a506788027
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-06-03 18:16:22 -0400

    logging

--- src/aind_data_schema/utils/examples_generator.py
+++ src/aind_data_schema/utils/examples_generator.py
@@ -4,6 +4,7 @@ import os
 import runpy
 from glob import glob
 from pathlib import Path
+import logging
 
 CURRENT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 ROOT_DIR = CURRENT_DIR.parents[2]
@@ -16,19 +17,19 @@ class ExamplesGenerator:
     def generate_all_examples(self):
         """Generate all examples in EXAMPLES_DIR"""
 
-        print(f"Running all examples in {EXAMPLES_DIR}")
+        logging.info(f"Running all examples in {EXAMPLES_DIR}")
         for example_file in glob(f"{EXAMPLES_DIR}/*.py"):
-            print(f"Running {example_file}")
+            logging.info(f"Running {example_file}")
             runpy.run_path(path_name=example_file)
 
     def generate_example(self, example_file):
         """Generate example from example_file"""
 
-        print(f"Running {example_file}")
+        logging.info(f"Running {example_file}")
         try:
             runpy.run_path(path_name=example_file)
         except Exception as e:
-            print(f"Error running {example_file}: {e}")
+            logging.info(f"Error running {example_file}: {e}")
 
 
 if __name__ == "__main__":
