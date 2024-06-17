1bd54298147859c03eae9234fdd24004872ecda6
Saskia de Vries <sejdevries@gmail.com>
2024-06-16 17:33:46 -0700

    linting

--- src/aind_data_schema/components/devices.py
+++ src/aind_data_schema/components/devices.py
@@ -8,7 +8,15 @@ from typing import List, Literal, Optional, Union
 from aind_data_schema_models.harp_types import HarpDeviceType
 from aind_data_schema_models.harp_types import Olfactometer as OlfactometerHarpType
 from aind_data_schema_models.organizations import InteruniversityMicroelectronicsCenter, Organization
-from aind_data_schema_models.units import FrequencyUnit, PowerUnit, SizeUnit, SpeedUnit, TemperatureUnit, UnitlessUnit, VoltageUnit
+from aind_data_schema_models.units import (
+    FrequencyUnit,
+    PowerUnit,
+    SizeUnit,
+    SpeedUnit,
+    TemperatureUnit,
+    UnitlessUnit,
+    VoltageUnit
+)
 from pydantic import Field, ValidationInfo, field_validator, model_validator
 from typing_extensions import Annotated
 
@@ -686,7 +694,8 @@ class PockelsCell(Device):
     """Description of a Pockels Cell"""
 
     device_type: Literal["Pockels cell"] = "Pockels cell"
-    polygonal_scanner: Optional[str] = Field(default=None, title="Polygonal scanner", description="Must match name of Polygonal scanner")
+    polygonal_scanner: Optional[str] = Field(default=None, title="Polygonal scanner", 
+                                             description="Must match name of Polygonal scanner")
     on_time: Optional[Decimal] = Field(default=None, title="On time (fraction of cycle)")
     off_time: Optional[Decimal] = Field(default=None, title="Off time (fraction of cycle)")
     time_setting_unit: UnitlessUnit = Field(default=UnitlessUnit.FC, title="time setting unit")
