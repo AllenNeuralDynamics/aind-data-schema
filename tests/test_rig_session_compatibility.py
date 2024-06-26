6be203ab3b8a2989405273e692e8ff2f5530ffd3
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2024-06-25 17:52:26 -0700

    compare daq names

--- tests/test_rig_session_compatibility.py
+++ tests/test_rig_session_compatibility.py
@@ -1,9 +1,9 @@
 """Tests rig session compatibility check"""
 import unittest
 
-from aind_data_schema.components.devices import MousePlatform
+from aind_data_schema.components.devices import MousePlatform, DAQDevice
 from aind_data_schema.core.rig import Rig
-from aind_data_schema.core.session import Session
+from aind_data_schema.core.session import Session, Stream
 from aind_data_schema.utils.compatibility_check import RigSessionCompatibility
 
 
@@ -12,12 +12,18 @@ class TestRigSessionCompatibility(unittest.TestCase):
 
     # TODO: use example rig and session once examples are synced
     mouse_platform = MousePlatform.model_construct(name="some_mouse_platform")
-    rig = Rig.model_construct(rig_id="some_rig_id", mouse_platform=mouse_platform)
-    compatible_session = Session.model_construct(rig_id="some_rig_id", mouse_platform_name="some_mouse_platform")
-    noncompatible_session = Session.model_construct(
-        rig_id="some_other_rig_id", mouse_platform_name="some_other_mouse_platform"
+    stream1 = Stream.model_construct(daq_names=["daq1", "daq2"])
+    stream2 = Stream.model_construct(daq_names=["daq3", "daq4"])
+    daq1 = DAQDevice.model_construct(name="daq1")
+    daq2 = DAQDevice.model_construct(name="daq2")
+    rig = Rig.model_construct(rig_id="some_rig_id", mouse_platform=mouse_platform, daqs=[daq1, daq2])
+    compatible_session = Session.model_construct(
+        rig_id="some_rig_id", mouse_platform_name="some_mouse_platform", data_streams=[stream1, stream1]
     )
-
+    noncompatible_session = Session.model_construct(
+        rig_id="some_other_rig_id",
+        mouse_platform_name="some_other_mouse_platform",
+    )
     checker = RigSessionCompatibility(rig=rig, session=compatible_session)
     noncompatible_checker = RigSessionCompatibility(rig=rig, session=noncompatible_session)
 
@@ -30,6 +36,11 @@ class TestRigSessionCompatibility(unittest.TestCase):
         """Tests compare mouse platform"""
         self.assertIsNone(self.checker._compare_mouse_platform_name())
         self.assertIsInstance(self.noncompatible_checker._compare_mouse_platform_name(), ValueError)
+
+    def test_compare_daq_names(self):
+        """Tests compare daq names"""
+        self.assertIsNone(self.checker._compare_daq_names())
+        self.assertIsInstance(self.noncompatible_checker._compare_daq_names(), ValueError)
 
     def test_run_compatibility_check(self):
         """Tests compatibility check"""
