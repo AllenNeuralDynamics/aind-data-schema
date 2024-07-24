a7d9c6651d7cd3476f3085d3c0e120e3afbe4898
Saskia de Vries <sejdevries@gmail.com>
2024-07-24 12:15:57 -0700

    moved QC into current schema

---
+++ src/aind_data_schema/core/quality_metrics.py
@@ -0,0 +1,47 @@
+""" Schemas for Quality Metrics """
+
+from __future__ import annotations
+
+from datetime import date
+from enum import Enum
+from typing import Any, Dict, List, Optional
+
+from pydantic import Field
+
+from aind_data_schema_models.modalities import Modality
+from aind_data_schema.base import AindCoreModel, AindGeneric, AindGenericType, AindModel
+
+
+class Status(Enum):
+    """QC Status"""
+
+    FAIL = "Fail"
+    PASS = "Pass"
+    UNKNOWN = "Unknown"
+
+
+class QCEvaluation(AindModel):
+    """Description of one evaluation stage"""
+
+    evaluation_modality: Modality = Field(..., title="Modality")
+    evaluation_stage: str = Field(..., title="Evaluation stage")
+    evaluator_full_name: str = Field(..., title="Evaluator full name")
+    evaluation_date: date = Field(..., title="Evaluation date")
+    qc_metrics: AindGenericType = Field(AindGeneric(), title="QC metrics")
+    stage_status: Status = Field(..., title="Stage status")
+    notes: Optional[str] = Field(None, title="Notes")
+
+
+class QualityMetrics(AindCoreModel):
+    """Description of quality metrics for a data asset"""
+
+    schema_version: str = Field(
+        "0.0.1",
+        description="schema version",
+        title="Schema Version",
+        const=True,
+    )
+    overall_status: Status = Field(..., title="Overall status")
+    overall_status_date: date = Field(..., title="Date of status")
+    evaluations: List[QCEvaluation] = Field(..., title="Evaluations")
+    notes: Optional[str] = Field(None, title="Notes")
---
+++ tests/test_quality_metrics.py
@@ -0,0 +1,41 @@
+"""test quality metrics """
+
+import datetime
+import unittest
+
+from pydantic import ValidationError
+
+from aind_data_schema_models.modalities import Modality
+from aind_data_schema.core.quality_metrics import QualityMetrics, QCEvaluation
+
+
+class QualityMetricsTests(unittest.TestCase):
+    """test quality metrics schema"""
+
+    def test_constructors(self):
+        """testing constructors"""
+
+        with self.assertRaises(ValidationError):
+            q = QualityMetrics()
+
+        now = datetime.datetime.now()
+
+        q = QualityMetrics(
+            overall_status_date=now.date(),
+            overall_status="Pass",
+            evaluations=[QCEvaluation(
+                evaluator_full_name="Bob",
+                evaluation_date=now.date(),
+                evaluation_modality=Modality.ECEPHYS,
+                evaluation_stage="Spike sorting",
+                qc_metrics={"number_good_units": [622]},
+                stage_status="Pass",
+                ),
+            ],
+        )
+
+        assert q is not None
+
+
+if __name__ == "__main__":
+    unittest.main()
