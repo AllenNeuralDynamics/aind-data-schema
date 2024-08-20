aef46a8adeff31c0226646d57c8ba15a0903c692
Jon Young <104453205+jtyoung84@users.noreply.github.com>
2024-08-20 10:25:20 -0700

    build: sets core metadata schema to v1.0.0

--- src/aind_data_schema/core/metadata.py
+++ src/aind_data_schema/core/metadata.py
@@ -48,7 +48,7 @@ class Metadata(AindCoreModel):
 
     _DESCRIBED_BY_URL = AindCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/metadata.py"
     describedBy: str = Field(_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
-    schema_version: Literal["1.0.1"] = Field("1.0.1")
+    schema_version: Literal["1.0.0"] = Field("1.0.0")
     id: UUID = Field(
         default_factory=uuid4,
         alias="_id",
