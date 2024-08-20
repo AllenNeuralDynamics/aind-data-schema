5385622d0b13e0035ae86a2c077e1f1aa42ef694
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2024-08-20 09:52:04 -0700

    BREAKING CHANGE: updates schemas to v1.0 (#1028)

--- src/aind_data_schema/core/data_description.py
+++ src/aind_data_schema/core/data_description.py
@@ -40,7 +40,7 @@ class DataDescription(AindCoreModel):
 
     _DESCRIBED_BY_URL = AindCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/data_description.py"
     describedBy: str = Field(_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
-    schema_version: Literal["0.13.12"] = Field("0.13.12")
+    schema_version: Literal["1.0.0"] = Field("1.0.0")
     license: Literal["CC-BY-4.0"] = Field("CC-BY-4.0", title="License")
 
     platform: Platform.ONE_OF = Field(
--- src/aind_data_schema/core/instrument.py
+++ src/aind_data_schema/core/instrument.py
@@ -35,7 +35,7 @@ class Instrument(AindCoreModel):
 
     _DESCRIBED_BY_URL = AindCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/instrument.py"
     describedBy: str = Field(_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
-    schema_version: Literal["0.10.29"] = Field("0.10.29")
+    schema_version: Literal["1.0.0"] = Field("1.0.0")
 
     instrument_id: Optional[str] = Field(
         None,
