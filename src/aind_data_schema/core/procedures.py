548222fc914606e359ab476605cc17a41c445d43
Saskia de Vries <sejdevries@gmail.com>
2024-08-26 09:22:25 -0700

    added Side

--- src/aind_data_schema/core/procedures.py
+++ src/aind_data_schema/core/procedures.py
@@ -560,6 +560,7 @@ class MyomatrixContact(AindModel):
     """ "Description of a contact on a myomatrix thread"""
 
     body_part: MouseAnatomicalStructure.BODY_PARTS = Field(..., title="Body part of contact insertion")
+    side: Side = Field(..., title="Body side")
     muscle: MouseAnatomicalStructure.EMG_MUSCLES = Field(..., title="Muscle of contact insertion")
     in_muscle: bool = Field(..., title="In muscle")
     notes: Optional[str] = Field(default=None, title="Notes")
@@ -650,7 +651,7 @@ class Procedures(AindCoreModel):
     _DESCRIBED_BY_URL = AindCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/procedures.py"
     describedBy: str = Field(_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
 
-    schema_version: Literal["1.0.0"] = Field("1.0.0")
+    schema_version: Literal["0.13.14"] = Field("0.13.14")
     subject_id: str = Field(
         ...,
         description="Unique identifier for the subject. If this is not a Allen LAS ID, indicate this in the Notes.",
