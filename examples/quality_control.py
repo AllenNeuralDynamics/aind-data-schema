e9e3c46f60c2b67030fca6871e49b49688c8f72a
Dan Birman <danbirman@gmail.com>
2024-09-26 10:50:01 -0700

    chore: lint

--- examples/quality_control.py
+++ examples/quality_control.py
@@ -13,93 +13,38 @@ eval0 = QCEvaluation(
     evaluation_description="Qualitative check that drift map shows minimal movement",
     evaluation_modality=Modality.ECEPHYS,
     evaluation_stage=Stage.PROCESSING,
-    evaluation_status=[
-        QCStatus(
-            evaluator="Fred Flintstone",
-            timestamp=t,
-            status=Status.FAIL
-        )
-    ],
+    evaluation_status=[QCStatus(evaluator="Fred Flintstone", timestamp=t, status=Status.FAIL)],
     qc_metrics=[
-        QCMetric(
-            name="Probe A drift",
-            value="High",
-            reference="ecephys-drift-map"
-        ),
-        QCMetric(
-            name="Probe B drift",
-            value="Low",
-            reference="ecephys-drift-map"
-        ),
-        QCMetric(
-            name="Probe C drift",
-            value="Low",
-            reference="ecephys-drift-map"
-        )
+        QCMetric(name="Probe A drift", value="High", reference="ecephys-drift-map"),
+        QCMetric(name="Probe B drift", value="Low", reference="ecephys-drift-map"),
+        QCMetric(name="Probe C drift", value="Low", reference="ecephys-drift-map"),
     ],
-    notes="Manually annotated: failed due to high drift on probe A"
+    notes="Manually annotated: failed due to high drift on probe A",
 )
 
 eval1 = QCEvaluation(
     evaluation_name="Video frame count check",
     evaluation_modality=Modality.BEHAVIOR_VIDEOS,
     evaluation_stage=Stage.RAW,
-    evaluation_status=[
-        QCStatus(
-            evaluator="Fred Flintstone",
-            timestamp=t,
-            status=Status.FAIL
-        )
-    ],
-    qc_metrics=[
-        QCMetric(
-            name="video_1_num_frames",
-            value=662
-        ),
-        QCMetric(
-            name="video_2_num_frames",
-            value=662
-        )
-    ],
-    notes="Pass when video_1_num_frames==video_2_num_frames"
+    evaluation_status=[QCStatus(evaluator="Fred Flintstone", timestamp=t, status=Status.FAIL)],
+    qc_metrics=[QCMetric(name="video_1_num_frames", value=662), QCMetric(name="video_2_num_frames", value=662)],
+    notes="Pass when video_1_num_frames==video_2_num_frames",
 )
 
 eval2 = QCEvaluation(
     evaluation_name="Probes present",
     evaluation_modality=Modality.ECEPHYS,
     evaluation_stage=Stage.RAW,
-    evaluation_status=[
-        QCStatus(
-            evaluator="Automated",
-            timestamp=t,
-            status=Status.PASS
-        )
-    ],
+    evaluation_status=[QCStatus(evaluator="Automated", timestamp=t, status=Status.PASS)],
     qc_metrics=[
-        QCMetric(
-            name="ProbeA_success",
-            value=True
-        ),
-        QCMetric(
-            name="ProbeB_success",
-            value=True
-        ),
-        QCMetric(
-            name="ProbeC_success",
-            value=True
-        )
+        QCMetric(name="ProbeA_success", value=True),
+        QCMetric(name="ProbeB_success", value=True),
+        QCMetric(name="ProbeC_success", value=True),
     ],
 )
 
 q = QualityControl(
-    overall_status=[
-        QCStatus(
-            evaluator="Automated",
-            timestamp=t,
-            status=Status.PASS
-        )
-    ],
-    evaluations=[eval0, eval1, eval2]
+    overall_status=[QCStatus(evaluator="Automated", timestamp=t, status=Status.PASS)], evaluations=[eval0, eval1, eval2]
 )
 
 serialized = q.model_dump_json()
