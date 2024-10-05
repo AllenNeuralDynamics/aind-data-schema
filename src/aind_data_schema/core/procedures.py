5a6c75bd43b509f451fed22591491ad36de3b54e
Dan Birman <danbirman@gmail.com>
2024-10-04 21:58:55 -0700

    Update dev from main v1.1.0 (#1096)
    * build: updates version number
    
    * feat: adds qc schema json
    
    * fix: updates schema version check to handle new core schemas
    
    * ci: adds service account token to github actions
    
    * bump schema version [skip actions]
    
    * fix: updates broken github action
    
    * bump schema version [skip actions]
    
    * build: increments version to 1.0.0
    
    * build: sets core metadata schema to v1.0.0
    
    * chore: version bump
    
    * bump schema version [skip actions]
    
    * chore: version bump for schemas (#1094)
    
    * chore: version bump for schemas
    
    * tests: example updates
    
    * bump schema version [skip actions]
    
    ---------
    
    Co-authored-by: Jon Young <104453205+jtyoung84@users.noreply.github.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/aind_data_schema/core/procedures.py
+++ src/aind_data_schema/core/procedures.py
@@ -649,7 +649,7 @@ class Procedures(AindCoreModel):
     _DESCRIBED_BY_URL = AindCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/procedures.py"
     describedBy: str = Field(_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
 
-    schema_version: Literal["1.0.0"] = Field("1.0.0")
+    schema_version: Literal["1.1.1"] = Field("1.1.1")
     subject_id: str = Field(
         ...,
         description="Unique identifier for the subject. If this is not a Allen LAS ID, indicate this in the Notes.",
