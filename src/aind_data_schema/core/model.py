9d6932920a79ca8abd936dd9e42d29f5b78f7755
Saskia de Vries <sejdevries@gmail.com>
2024-10-16 16:01:12 -0700

    added more to model

--- src/aind_data_schema/core/model.py
+++ src/aind_data_schema/core/model.py
@@ -2,11 +2,12 @@
 
 from decimal import Decimal
 from enum import Enum
-from typing import List, Literal, Optional
+from typing import List, Literal, Optional, Union
 
 from pydantic import Field
 
 from aind_data_schema_models.modalities import Modality
+from aind_data_schema_models.organizations import Organization
 
 from aind_data_schema.base import AindCoreModel, AindGenericType, AindModel, AwareDatetimeWithDefault
 from aind_data_schema.components.devices import Software
@@ -26,14 +27,22 @@ class ModelArchitecture(AindModel):
     backbone: Backbone = Field(..., title="Backbone")
     layers: int = Field(..., title="Layers")
     parameters: AindGenericType = Field(..., title="Parameters")
+    #notes?
 
 
-class ModelPerformance(AindModel):
+class ScoreStatistics(AindModel):
+    """Statistics for x-fold validation scores"""
+
+    mean: Decimal = Field(..., title="Mean")
+    std: Decimal = Field(..., title="Standard deviation")
+
+
+class PerformanceScore(AindModel):
     """Description of performance metrics"""
 
-    precision: Decimal = Field(..., title="Precision")
-    recall: Decimal = Field(..., title="Recall")
-    f1_score: Decimal = Field(..., title="F1 score")
+    precision: Union[Decimal, ScoreStatistics] = Field(..., title="Precision")
+    recall: Union[Decimal, ScoreStatistics] = Field(..., title="Recall")
+    f1_score: Union[Decimal, ScoreStatistics] = Field(..., title="F1 score")
 
 
 class ModelTraining(AindModel):
@@ -41,13 +50,20 @@ class ModelTraining(AindModel):
 
     training_data: str = Field(..., title="Path to training data")
     training_data_description: Optional[str] = Field(default=None, title="Description of training data") 
+    training_date: AwareDatetimeWithDefault = Field(..., title="Date trained") #not sure we need datetime
     validation_folds: int = Field(..., title="Validation folds") #is the validation methods x-fold? or are there other validations? Enum?
-    performance: ModelPerformance = Field(..., title="Training performance")
+    performance: PerformanceScore = Field(..., title="Training performance")
+    notes: Optional[str] = Field(default=None, title="Notes")
 
 
 class ModelEvaluation(AindModel):
     """Description of model evaluation"""
 
+    evaluation_data: str = Field(..., title="Path to evaluation data")
+    evaluation_data_description: Optional[str] = Field(default=None, title="Description of evaluation data")
+    evaluation_date: AwareDatetimeWithDefault = Field(..., title="Date trained") #not sure we need datetime
+    performance: PerformanceScore = Field(..., title="Evaluation performance")
+    notes: Optional[str] = Field(default=None, title="Notes")
 
 
 class Model(AindCoreModel):
@@ -58,15 +74,13 @@ class Model(AindCoreModel):
     schema_version: Literal["1.0.1"] = Field("0.0.1")
 
     name: str = Field(..., title="Name")
-    developer_full_name: str = Field(..., title="Name of developer")
-    #developer_institution: 
-    #shared by
+    developer_full_name: Optional[str] = Field(default=None, title="Name of developer")
+    developer_institution: Organization.ONE_OF = Field(default=None, title="Institute where developed")
     modality: Modality.ONE_OF = Field(..., title="Modality")
-    date_trained: AwareDatetimeWithDefault = Field(..., title="Date trained") #not sure we need datetime
     model_architecture: ModelArchitecture = Field(..., title="Model architecture")
     software: Software = Field(..., title="software")
     direct_use: str = Field(..., title="Intended model use", description="Semantic description of intended use")
     limitations: Optional[str] = Field(default=None, title="Model limitations")
     training: List[ModelTraining] = Field(..., title="Training")
-
-
+    evaluations: Optional[List[ModelEvaluation]] = Field(default=[], title="Evaluations")
+    notes: Optional[str] = Field(default=None, title="Notes")
