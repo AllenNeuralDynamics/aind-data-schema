4a82d15c8b7cd81b9532ef80108f41cb6b9131b6
Dan Birman <danbirman@gmail.com>
2024-11-21 09:43:32 -0800

    feat: improving extension testing

--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -1,6 +1,8 @@
+from typing import Any, Literal
 import unittest
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
+from aind_data_schema.base import AindCoreModel, AindGenericType
 from aind_data_schema.core.quality_control import QualityControl
 
 
@@ -10,14 +12,35 @@ class ExtensionTests(unittest.TestCase):
     def test_extension(self):
         """run through each example, compare to rendered json"""
 
-        class QCExtension(BaseModel):
+        # Ensure that an extension can be added to the QualityControl object
+        class Extension(BaseModel):
             """Extension for QualityControl"""
 
+            ext_field: str = Field(..., title="Example")
+
+        class Parent(AindCoreModel):
+            """Parent class"""
+            describedBy: Literal["url"] = Field(default="url")
+            schema_version: Literal["1.2.1"] = Field(default="1.2.1")
+
             example: str = Field(..., title="Example")
+            extensions: Extension = Field(default=None, title="Extensions")
 
-        qcext = QCExtension(example="example")
+        ext = Extension(ext_field="ext_field")
 
-        qc = QualityControl(
-            evaluations = [],
-            extensions=qcext
+        parent = Parent(
+            example="example",
+            extensions=ext
         )
+        print(parent.model_dump_json())
+
+        data = parent.model_dump()
+        parent = Parent(**data)
+        print(parent.extensions)
+        print(parent.model_dump_json())
+
+        self.assertIsNotNone(parent)
+
+
+if __name__ == "__main__":
+    unittest.main()
