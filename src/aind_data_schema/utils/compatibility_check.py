6503010c646f4eb4653e5200ffc2ea4a3a95d16f
Dan Birman <danbirman@gmail.com>
2025-01-27 14:33:24 -0800

    tests: final fixes to compatibility check code

--- src/aind_data_schema/utils/compatibility_check.py
+++ src/aind_data_schema/utils/compatibility_check.py
@@ -2,6 +2,7 @@
 
 from typing import Optional
 
+from aind_data_schema.components.devices import CameraAssembly, EphysAssembly
 from aind_data_schema.core.instrument import RewardDelivery, Instrument
 from aind_data_schema.core.session import Session
 
@@ -36,11 +37,11 @@ class InstrumentSessionCompatibility:
         session_daqs = [
             daq for stream in getattr(self.session, "data_streams", []) for daq in getattr(stream, "daq_names", [])
         ]
-        instrument_daqs = [getattr(daq, "name", None) for daq in getattr(self.inst, "daqs", [])]
-        if not set(session_daqs).issubset(set(instrument_daqs)):
+        instrument_component_names = [getattr(comp, "name", None) for comp in getattr(self.inst, "components", [])]
+        if not set(session_daqs).issubset(set(instrument_component_names)):
             return ValueError(
                 f"daq names in session do not match daq names in inst. "
-                f"session_daqs: {set(session_daqs)} instrument_daqs: {set(instrument_daqs)}"
+                f"session_daqs: {set(session_daqs)} instrument components: {set(instrument_component_names)}"
             )
 
     def _compare_camera_names(self) -> Optional[ValueError]:
@@ -50,15 +51,12 @@ class InstrumentSessionCompatibility:
             for stream in getattr(self.session, "data_streams", [])
             for camera in getattr(stream, "camera_names", [])
         ]
-        instrument_cameras = [
-            name
-            for camera_device in getattr(self.inst, "cameras", [])
-            for name in (camera_device.camera.name, camera_device.name)
-        ]
-        if not set(session_cameras).issubset(set(instrument_cameras)):
+        instrument_camera_names = [comp.camera.name if isinstance(comp, CameraAssembly) else None for comp in getattr(self.inst, "components", [])]
+
+        if not set(session_cameras).issubset(set(instrument_camera_names)):
             return ValueError(
                 f"camera names in session do not match camera names in inst. "
-                f"session_cameras: {set(session_cameras)} instrument_cameras: {set(instrument_cameras)}"
+                f"session_cameras: {set(session_cameras)} instrument_cameras: {set(instrument_camera_names)}"
             )
 
     def _compare_light_sources(self) -> Optional[ValueError]:
@@ -68,14 +66,12 @@ class InstrumentSessionCompatibility:
             for stream in getattr(self.session, "data_streams", [])
             for light_source in getattr(stream, "light_sources", [])
         ]
-        instrument_light_sources = [
-            getattr(light_source, "name", None) for light_source in getattr(self.inst, "light_sources", [])
-        ]
-        if not set(session_light_sources).issubset(set(instrument_light_sources)):
+        instrument_component_names = [getattr(comp, "name", None) for comp in getattr(self.inst, "components", [])]
+        if not set(session_light_sources).issubset(set(instrument_component_names)):
             return ValueError(
                 f"light source names in session do not match light source names in inst. "
                 f"session_light_sources: {set(session_light_sources)} "
-                f"instrument_light_sources: {set(instrument_light_sources)}"
+                f"instrument_light_sources: {set(instrument_component_names)}"
             )
 
     def _compare_ephys_assemblies(self) -> Optional[ValueError]:
@@ -85,14 +81,13 @@ class InstrumentSessionCompatibility:
             for stream in getattr(self.session, "data_streams", [])
             for ephys_module in getattr(stream, "ephys_modules")
         ]
-        instrument_ephys_assemblies = [
-            ephys_assembly.name for ephys_assembly in getattr(self.inst, "ephys_assemblies", [])
-        ]
-        if not set(session_ephys_assemblies).issubset(set(instrument_ephys_assemblies)):
+        instrument_component_names = [comp.name if isinstance(comp, EphysAssembly) else None for comp in getattr(self.inst, "components", [])]
+
+        if not set(session_ephys_assemblies).issubset(set(instrument_component_names)):
             return ValueError(
                 f"ephys assembly names in session do not match ephys assembly names in inst. "
                 f"session_ephys_assemblies: {set(session_ephys_assemblies)}"
-                f" instrument_ephys_assemblies: {set(instrument_ephys_assemblies)}"
+                f" instrument_ephys_assemblies: {set(instrument_component_names)}"
             )
 
     def _compare_stick_microscopes(self) -> Optional[ValueError]:
@@ -102,16 +97,13 @@ class InstrumentSessionCompatibility:
             for stream in getattr(self.session, "data_streams", [])
             for stick_microscope in getattr(stream, "stick_microscopes", [])
         ]
-        instrument_stick_microscopes = [
-            name
-            for camera_device in getattr(self.inst, "stick_microscopes", [])
-            for name in (camera_device.camera.name, camera_device.name)
-        ]
-        if not set(session_stick_microscopes).issubset(set(instrument_stick_microscopes)):
+        instrument_camera_names = [comp.camera.name if isinstance(comp, CameraAssembly) else None for comp in getattr(self.inst, "components", [])]
+
+        if not set(session_stick_microscopes).issubset(set(instrument_camera_names)):
             return ValueError(
                 f"stick microscope names in session do not match stick microscope names in inst. "
                 f"session_stick_microscopes: {set(session_stick_microscopes)} "
-                f"instrument_stick_microscopes: {set(instrument_stick_microscopes)}"
+                f"instrument_stick_microscopes: {set(instrument_camera_names)}"
             )
 
     def _compare_manipulator_modules(self) -> Optional[ValueError]:
@@ -121,14 +113,13 @@ class InstrumentSessionCompatibility:
             for stream in getattr(self.session, "data_streams", [])
             for manipulator_module in getattr(stream, "manipulator_modules", [])
         ]
-        instrument_manipulator_modules = [
-            laser_assembly.name for laser_assembly in getattr(self.inst, "laser_assemblies", [])
-        ]
-        if not set(session_manipulator_modules).issubset(set(instrument_manipulator_modules)):
+        instrument_component_names = [getattr(comp, "name", None) for comp in getattr(self.inst, "components", [])]
+
+        if not set(session_manipulator_modules).issubset(set(instrument_component_names)):
             return ValueError(
                 f"manipulator module names in session do not match manipulator names (laser assemblies) in inst. "
                 f"session_manipulators: {set(session_manipulator_modules)}"
-                f" instrument_manipulators: {set(instrument_manipulator_modules)}"
+                f" instrument_manipulators: {set(instrument_component_names)}"
             )
 
     def _compare_detectors(self) -> Optional[ValueError]:
@@ -138,11 +129,12 @@ class InstrumentSessionCompatibility:
             for stream in getattr(self.session, "data_streams", [])
             for detector in getattr(stream, "detectors", [])
         ]
-        instrument_detectors = [detector.name for detector in getattr(self.inst, "detectors", [])]
-        if not set(session_detectors).issubset(set(instrument_detectors)):
+        instrument_component_names = [getattr(comp, "name", None) for comp in getattr(self.inst, "components", [])]
+
+        if not set(session_detectors).issubset(set(instrument_component_names)):
             return ValueError(
                 f"detector names in session do not match detector names in inst. "
-                f"session_detectors: {set(session_detectors)} instrument_detectors: {set(instrument_detectors)}"
+                f"session_detectors: {set(session_detectors)} instrument_detectors: {set(instrument_component_names)}"
             )
 
     def _compare_patch_cords(self) -> Optional[ValueError]:
@@ -152,11 +144,12 @@ class InstrumentSessionCompatibility:
             for stream in getattr(self.session, "data_streams", [])
             for fiber_connection in getattr(stream, "fiber_connections", [])
         ]
-        instrument_patch_cords = [patch_cord.name for patch_cord in getattr(self.inst, "patch_cords", [])]
-        if not set(session_patch_cords).issubset(set(instrument_patch_cords)):
+        instrument_component_names = [getattr(comp, "name", None) for comp in getattr(self.inst, "components", [])]
+
+        if not set(session_patch_cords).issubset(set(instrument_component_names)):
             return ValueError(
                 f"patch cord names in session do not match patch cord names in inst. "
-                f"session_patch_cords: {set(session_patch_cords)} instrument_patch_cords: {set(instrument_patch_cords)}"
+                f"session_patch_cords: {set(session_patch_cords)} instrument_patch_cords: {set(instrument_component_names)}"
             )
 
     def _compare_fiber_modules(self) -> Optional[ValueError]:
@@ -166,14 +159,13 @@ class InstrumentSessionCompatibility:
             for stream in getattr(self.session, "data_streams", [])
             for fiber_module in getattr(stream, "fiber_modules", [])
         ]
-        instrument_fiber_modules = [
-            fiber_assembly.name for fiber_assembly in getattr(self.inst, "fiber_assemblies", [])
-        ]
-        if not set(session_fiber_modules).issubset(set(instrument_fiber_modules)):
+        instrument_component_names = [getattr(comp, "name", None) for comp in getattr(self.inst, "components", [])]
+
+        if not set(session_fiber_modules).issubset(set(instrument_component_names)):
             return ValueError(
                 f"fiber module names in session do not match fiber assembly names in inst. "
                 f"session_fiber_modules: {set(session_fiber_modules)} "
-                f"instrument_fiber_assemblies: {set(instrument_fiber_modules)}"
+                f"instrument_fiber_assemblies: {set(instrument_component_names)}"
             )
 
     def _compare_stimulus_devices(self) -> Optional[ValueError]:
@@ -183,21 +175,13 @@ class InstrumentSessionCompatibility:
             for stimulus_epoch in getattr(self.session, "stimulus_epochs", [])
             for stimulus_device_name in getattr(stimulus_epoch, "stimulus_device_names")
         ]
-        instrument_stimulus_devices = [
-            stimulus_device.name
-            for stimulus_device in getattr(self.inst, "stimulus_devices", [])
-            if not isinstance(stimulus_device, RewardDelivery)
-        ] + [
-            reward_spout.name
-            for stimulus_device in getattr(self.inst, "stimulus_devices", [])
-            if isinstance(stimulus_device, RewardDelivery)
-            for reward_spout in getattr(stimulus_device, "reward_spouts", [])
-        ]
-        if not set(session_stimulus_devices).issubset(set(instrument_stimulus_devices)):
+        instrument_component_names = [getattr(comp, "name", None) for comp in getattr(self.inst, "components", [])]
+
+        if not set(session_stimulus_devices).issubset(set(instrument_component_names)):
             return ValueError(
                 f"stimulus device names in session do not match stimulus device names in inst. "
                 f"session_stimulus_devices: {set(session_stimulus_devices)} "
-                f"instrument_stimulus_devices: {set(instrument_stimulus_devices)}"
+                f"instrument_stimulus_devices: {set(instrument_component_names)}"
             )
 
     def run_compatibility_check(self) -> None:
