b2f0f61b4335f9f1ccffda2b84b6ea02dd5f51e3
Dan Birman <danbirman@gmail.com>
2025-03-27 16:01:58 -0700

    refactor: remove UNITS union

--- src/aind_data_schema/components/measurements.py
+++ src/aind_data_schema/components/measurements.py
@@ -3,44 +3,14 @@
 from typing import Annotated, List, Literal, Optional, Union
 
 from aind_data_schema_models.units import (
-    AngleUnit,
-    ConcentrationUnit,
-    CurrentUnit,
-    FrequencyUnit,
-    MassUnit,
-    MemoryUnit,
     PowerUnit,
-    SizeUnit,
-    SoundIntensityUnit,
-    SpeedUnit,
-    TemperatureUnit,
     TimeUnit,
-    UnitlessUnit,
-    VoltageUnit,
     VolumeUnit,
 )
 
 from aind_data_schema.base import AwareDatetimeWithDefault, Field
 from aind_data_schema.components.configs import DeviceConfig
 from aind_data_schema.components.reagent import Reagent
-
-UNITS = Union[
-    SizeUnit,
-    MassUnit,
-    FrequencyUnit,
-    SpeedUnit,
-    VolumeUnit,
-    AngleUnit,
-    TimeUnit,
-    PowerUnit,
-    CurrentUnit,
-    ConcentrationUnit,
-    TemperatureUnit,
-    SoundIntensityUnit,
-    VoltageUnit,
-    MemoryUnit,
-    UnitlessUnit,
-]
 
 
 class Calibration(DeviceConfig):
