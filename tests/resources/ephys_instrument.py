34848b0290e39caa42173a9e8f997360deda0892
Dan Birman <danbirman@gmail.com>
2025-03-28 11:10:54 -0700

    tests: wavelength units for Filter

--- tests/resources/ephys_instrument.py
+++ tests/resources/ephys_instrument.py
@@ -231,7 +231,8 @@ filt = Filter(
     name="LP filter",
     filter_type="Long pass",
     manufacturer=Organization.THORLABS,
-    description="850 nm longpass filter",
+    cut_on_wavelength=850,
+    wavelength_unit=SizeUnit.NM,
 )
 
 lens = Lens(
