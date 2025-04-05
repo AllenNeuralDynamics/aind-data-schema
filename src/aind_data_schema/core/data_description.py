f1ced8b228a2420808e4d6f6056e9bcb0038fcea
Tom Chartrand <tom.chartrand@alleninstitute.org>
2025-04-04 17:33:37 -0700

    fix: license edit lost in merge

--- src/aind_data_schema/core/data_description.py
+++ src/aind_data_schema/core/data_description.py
@@ -35,7 +35,7 @@ class DataDescription(DataCoreModel):
     _DESCRIBED_BY_URL = DataCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/data_description.py"
     describedBy: str = Field(default=_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
     schema_version: SkipValidation[Literal["2.0.8"]] = Field(default="2.0.8")
-    license: Literal["CC-BY-4.0"] = Field(default="CC-BY-4.0", title="License")
+    license: License = Field(default=License.CC_BY_40, title="License")
 
     subject_id: Optional[str] = Field(
         default=None,
