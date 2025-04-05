44da2e6623f98b596a5cb025a644b1fa468ead0e
Saskia de Vries <sejdevries@gmail.com>
2025-04-04 17:03:36 -0700

    bergamo example

--- examples/bergamo_ophys_acquisition.py
+++ examples/bergamo_ophys_acquisition.py
@@ -14,9 +14,10 @@ from aind_data_schema.core.acquisition import (
     SubjectDetails,
 )
 from aind_data_schema.components.configs import (
+    Channel,
     DetectorConfig,
-    FieldOfView,
     LaserConfig,
+    SinglePlaneFieldOfView,
     StimulusModality,
 )
 from aind_data_schema_models.brain_atlas import CCFStructure
@@ -47,20 +48,25 @@ a = Acquisition(
                 "Face Camera",
             ],
             configurations=[
-                LaserConfig(
-                    device_name="Laser A",
-                    wavelength=405,
-                    wavelength_unit="nanometer",
-                    excitation_power=10,
-                    excitation_power_unit="milliwatt",
-                ),
-                DetectorConfig(
-                    device_name="PMT A",
-                    exposure_time=0.1,
-                    trigger_type="Internal",
-                ),
-                FieldOfView(
-                    index=0,
+                SinglePlaneFieldOfView(
+                    channel=Channel(
+                        channel_name="Green channel",
+                        intended_measurement="GCaMP",
+                        detector_configuration=DetectorConfig(
+                            device_name="PMT A",
+                            exposure_time=0.1,
+                            trigger_type="Internal",
+                        ),
+                        light_source_configurations=[
+                            LaserConfig(
+                                device_name="Laser A",
+                                wavelength=405,
+                                wavelength_unit="nanometer",
+                                excitation_power=10,
+                                excitation_power_unit="milliwatt",
+                            ),
+                        ],
+                    ),
                     imaging_depth=150,
                     targeted_structure=CCFStructure.MOP,
                     fov_coordinate_ml=1.5,
