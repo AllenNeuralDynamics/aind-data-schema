b6572e84a3d450d8cb406eb962728256e78395a3
Saskia de Vries <sejdevries@gmail.com>
2025-04-05 15:56:42 -0700

    ophys fib example

--- examples/ophys_acquisition.py
+++ examples/ophys_acquisition.py
@@ -10,8 +10,7 @@ from aind_data_schema.core.acquisition import (
     DataStream,
     SubjectDetails,
 )
-from aind_data_schema.components.configs import DetectorConfig, PatchCordConfig, LaserConfig
-from aind_data_schema.components.configs import Channel
+from aind_data_schema.components.configs import Channel, DetectorConfig, PatchCordConfig, LaserConfig
 
 t = datetime(2022, 7, 12, 7, 00, 00, tzinfo=timezone.utc)
 
@@ -39,55 +38,51 @@ a = Acquisition(
                 "Patch Cord B",
             ],
             configurations=[
-                LaserConfig(
-                    device_name="Laser A",
-                    wavelength=405,
-                    wavelength_unit="nanometer",
-                    excitation_power=10,
-                    excitation_power_unit="milliwatt",
-                ),
-                LaserConfig(
-                    device_name="Laser B",
-                    wavelength=473,
-                    wavelength_unit="nanometer",
-                    excitation_power=7,
-                    excitation_power_unit="milliwatt",
-                ),
-                DetectorConfig(device_name="Hamamatsu Camera", exposure_time=10, trigger_type="Internal"),
                 PatchCordConfig(
                     device_name="Patch Cord A",
                     output_power=40,
                     output_power_unit="microwatt",
                     fiber_name="Fiber A",
-                    channel=Channel(
-                        channel_name="Channel A",
-                        intended_measurement="Dopamine",
-                        light_source_name="Laser A",
-                        filter_names=["Excitation filter 410nm"],
-                        detector_name="Green CMOS",
-                        excitation_wavelength=410,
-                        excitation_power=10,
-                        emission_wavelength=600,
-                    ),
+                    channels=[
+                        Channel(
+                            channel_name="Channel A",
+                            intended_measurement="Dopamine",
+                            detector_configuration=DetectorConfig(device_name="Hamamatsu Camera", exposure_time=10, trigger_type="Internal"),
+                            light_source_configuration=[
+                                LaserConfig(
+                                    device_name="Laser A",
+                                    wavelength=405,
+                                    wavelength_unit="nanometer",
+                                    excitation_power=10,
+                                    excitation_power_unit="milliwatt",
+                                ),
+                            ],
+                        ),
+                    ],
                 ),
                 PatchCordConfig(
                     device_name="Patch Cord B",
                     output_power=43,
                     output_power_unit="microwatt",
                     fiber_name="Fiber B",
-                    channel=Channel(
-                        channel_name="Channel B",
-                        intended_measurement="Dopamine",
-                        light_source_name="Laser B",
-                        filter_names=["Excitation filter 560nm"],
-                        detector_name="Red CMOS",
-                        excitation_wavelength=560,
-                        excitation_power=7,
-                        emission_wavelength=700,
-                    ),
+                    channels=[
+                        Channel(
+                            channel_name="Channel B",
+                            intended_measurement="GCaMP",
+                            detector_configuration=DetectorConfig(device_name="Hamamatsu Camera", exposure_time=10, trigger_type="Internal"),
+                            light_source_configuration=[
+                                LaserConfig(
+                                    device_name="Laser B",
+                                    wavelength=473,
+                                    wavelength_unit="nanometer",
+                                    excitation_power=7,
+                                    excitation_power_unit="milliwatt",
+                                ),
+                            ],
+                        )
+                    ]
                 ),
             ],
-            notes="Internal trigger.",
         )
     ],
 )
