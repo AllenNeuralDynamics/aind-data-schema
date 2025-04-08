ef7218f9cad499cf26401c6b81db755fa1c28a47
Dan Birman <danbirman@gmail.com>
2025-04-07 21:16:52 -0700

    chore: lint

--- examples/exaspim_acquisition.py
+++ examples/exaspim_acquisition.py
@@ -8,8 +8,15 @@ from aind_data_schema_models.registries import Registry
 from aind_data_schema_models.units import PowerUnit, SizeUnit
 from aind_data_schema_models.modalities import Modality
 
-from aind_data_schema.components import tile
-from aind_data_schema.components.configs import Channel, DeviceConfig, LaserConfig, Image, Immersion, InVitroImagingConfig
+from aind_data_schema.components.configs import (
+    Channel,
+    DeviceConfig,
+    LaserConfig,
+    Image,
+    Immersion,
+    InVitroImagingConfig,
+    DetectorConfig,
+)
 from aind_data_schema.components.coordinates import CoordinateSystemLibrary, Scale, CoordinateTransform
 from aind_data_schema.components.identifiers import Person
 from aind_data_schema.core.acquisition import Acquisition, DataStream
@@ -45,12 +52,13 @@ invitro_config = InVitroImagingConfig(
         Channel(
             channel_name="488",
             intended_measurement="GFP signal",
-            light_sources=[LaserConfig(
-                device_name="LAS_08308",
-                wavelength=488,
-                wavelength_unit=SizeUnit.NM,
-                excitation_power=200,
-                excitation_power_unit=PowerUnit.MW,
+            light_sources=[
+                LaserConfig(
+                    device_name="LAS_08308",
+                    wavelength=488,
+                    wavelength_unit=SizeUnit.NM,
+                    excitation_power=200,
+                    excitation_power_unit=PowerUnit.MW,
                 ),
             ],
             emission_filters=[
@@ -80,8 +88,8 @@ invitro_config = InVitroImagingConfig(
                 )
             ],
             detector_configuration=DetectorConfig(
-                device_name="PMT_1".
-            )
+                device_name="PMT_1",
+            ),
         ),
     ],
     images=[image0, image1],
@@ -130,8 +138,13 @@ acq = Acquisition(
             stream_start_time=t,
             stream_end_time=t,
             modalities=[Modality.SPIM],
-            active_devices=["LAS_08308","539251",],
-            configurations=[invitro_config,],
+            active_devices=[
+                "LAS_08308",
+                "539251",
+            ],
+            configurations=[
+                invitro_config,
+            ],
         )
     ],
     acquisition_start_time=t,
