2041924f22a9f9bcaaee616e99df406728ea34f4
Dan Birman <danbirman@gmail.com>
2025-04-14 21:56:14 -0700

    fix: fix examples generator
    Runs kind of slow now

--- src/aind_data_schema/utils/examples_generator.py
+++ src/aind_data_schema/utils/examples_generator.py
@@ -2,10 +2,9 @@
 
 import logging
 import os
-import runpy
 from glob import glob
 from pathlib import Path
-from concurrent.futures import ThreadPoolExecutor
+import subprocess
 
 CURRENT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 ROOT_DIR = CURRENT_DIR.parents[2]
@@ -16,32 +15,21 @@ class ExamplesGenerator:
     """Class to generate example files from .py files in examples directory"""
 
     def generate_all_examples(self):
-        """Generate all examples in EXAMPLES_DIR in parallel"""
+        """Generate all examples in EXAMPLES_DIR"""
 
         logging.info(f"Running all examples in {EXAMPLES_DIR}")
-
-        def run_example(example_file):  # pragma: no cover
-            """Run example file"""
-            if "__init__" in example_file:
-                return
-            logging.info(f"Running {example_file}")
-            try:
-                runpy.run_path(path_name=example_file)
-            except Exception as e:
-                logging.info(f"Error running {example_file}: {e}")
-
-        example_files = [f for f in glob(f"{EXAMPLES_DIR}/*.py") if "__init__" not in f]
-
-        with ThreadPoolExecutor() as executor:
-            executor.map(run_example, example_files)
+        for example_file in glob(str(EXAMPLES_DIR / "*.py")):
+            if Path(example_file).name == "__init__.py":
+                continue
+            self.generate_example(example_file)
 
     def generate_example(self, example_file):
         """Generate example from example_file"""
 
         logging.info(f"Running {example_file}")
         try:
-            runpy.run_path(path_name=example_file)
-        except Exception as e:
+            subprocess.run(["python", example_file], check=True)
+        except subprocess.CalledProcessError as e:
             logging.info(f"Error running {example_file}: {e}")
 
 
