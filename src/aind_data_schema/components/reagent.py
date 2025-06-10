fd1853e857cce02f317eb6b67c2a702ad199314a
Saskia de Vries <sejdevries@gmail.com>
2025-06-10 10:24:41 -0700

    updated stains

--- src/aind_data_schema/components/reagent.py
+++ src/aind_data_schema/components/reagent.py
@@ -29,20 +29,20 @@ class StainType(str, Enum):
     FILL = "Fill"
 
 
-class Fluorophore(str, Enum):
+class FluorophoreType(str, Enum):
+    """Fluorophore types"""
+
+    ALEXA = "Alexa Fluor"
+    ATTO = "ATTO"
+    CYANINE = "Cyanine"
+
+
+class Fluorophore(DataModel):
     """Fluorophores used in HCR and Immunolabeling"""
 
-    ALEXA_405 = "Alexa Fluor 405"
-    ALEXA_488 = "Alexa Fluor 488"
-    ALEXA_546 = "Alexa Fluor 546"
-    ALEXA_568 = "Alexa Fluor 568"
-    ALEXA_594 = "Alexa Fluor 594"
-    ALEXA_633 = "Alexa Fluor 633"
-    ALEXA_647 = "Alexa Fluor 647"
-    ATTO_488 = "ATTO 488"
-    ATTO_565 = "ATTO 565"
-    ATTO_643 = "ATTO 643"
-    CY3 = "Cyanine Cy 3"
+    fluorophore_type: FluorophoreType = Field(..., title="Fluorophore type") #I think we want a different field name
+    excitation_wavelength: int = Field(..., title="Excitation wavelength (nm)")
+    excitation_wavelength_unit: SizeUnit = Field(default=SizeUnit.NM, title="Excitation wavelength unit")
 
 
 class Reagent(DataModel):
@@ -55,12 +55,12 @@ class Reagent(DataModel):
     expiration_date: Optional[date] = Field(default=None, title="Lot expiration date")
 
 
-class Stain(Reagent):
-    """Description of a non-oligo probe stain"""
+# class Stain(Reagent):
+#     """Description of a non-oligo probe stain"""
 
-    stain_type: StainType = Field(..., title="Stain type")
-    concentration: float = Field(..., title="Concentration")
-    concentration_unit: ConcentrationUnit = Field(default=ConcentrationUnit.UM, title="Concentration unit")
+#     stain_type: StainType = Field(..., title="Stain type")
+#     concentration: float = Field(..., title="Concentration")
+#     concentration_unit: ConcentrationUnit = Field(default=ConcentrationUnit.UM, title="Concentration unit")
 
 
 class Antibody(Reagent):
@@ -94,27 +94,57 @@ class OligoProbeSet(Reagent):
     """set of probes used in BarSEQ"""
 
     gene_probes: List[GeneProbes] = Field(..., title="Gene probes")
-    # IDT scale stuff?
 
+class HCRProbe(Reagent):
+    """Oligonucleotide probe used in HCR"""
 
-class Readout(Reagent):
-    """Description of a readout"""
-
-    fluorophore: Fluorophore = Field(..., title="Fluorophore")
-    excitation_wavelength: int = Field(..., title="Excitation wavelength (nm)")
-    excitation_wavelength_unit: SizeUnit = Field(default=SizeUnit.NM, title="Excitation wavelength unit")
-    stain_type: StainType = Field(..., title="Stain type")
-
-
-class HCRReadout(Readout):
-    """Description of a readout for HCR"""
-
+    gene: PIDName = Field(..., title="Gene name")
     initiator_name: str = Field(..., title="Initiator name")
 
 
-class GeneticStain(Reagent):
-    """Description of an oligonucleotide probe(s) targeting a gene and readout"""
+# class Readout(Reagent):
+#     """Description of a readout"""
 
-    gene_probe: GeneProbes = Field(..., title="Gene probe")
-    readout: Discriminated[Readout | HCRReadout] = Field(..., title="Readout")
+#     fluorophore: Fluorophore = Field(..., title="Fluorophore")
+#     stain_type: StainType = Field(..., title="Stain type")
+
+
+# class HCRReadout(Readout):
+#     """Description of a readout for HCR"""
+
+#     initiator_name: str = Field(..., title="Initiator name")
+
+
+# class GeneticStain(Reagent):
+#     """Description of an oligonucleotide probe(s) targeting a gene and readout"""
+
+#     gene_probe: GeneProbes = Field(..., title="Gene probe")
+#     readout: Discriminated[Readout | HCRReadout] = Field(..., title="Readout")
+#     species: Species.ONE_OF = Field(..., title="Species")
+
+
+class ProteinProbe(Reagent):
+    """Description of a protein probe"""
+
+    protein: PIDName = Field(..., title="Target protein name")
+    mass: float = Field(..., title="Mass of protein probe")
+    mass_unit: MassUnit = Field(default=MassUnit.UG, title="Mass unit")
+    sequence: Optional[str] = Field(default=None, title="Amino acid sequence")
+
+
+class SmallMoleculeProbe(Reagent):
+    """Description of a small molecule probe"""
+
+    molecule: Optional[PIDName] = Field(default=None, title="Target small molecule name")
+    mass: float = Field(..., title="Mass of small molecule probe")
+    mass_unit: MassUnit = Field(default=MassUnit.UG, title="Mass unit")
+
+
+class Stain(Reagent):
+    """Description of a stain"""
+
+    target: Discriminated[HCRProbe | ProteinProbe | SmallMoleculeProbe] == Field(..., title="Target")
+    fluorophore: Fluorophore = Field(..., title="Fluorophore")
+    stain_type: StainType = Field(..., title="Stain type")
     species: Species.ONE_OF = Field(..., title="Species")
+
