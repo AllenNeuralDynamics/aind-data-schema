7eff69ff1de6248bc5acc5771baaeaafdff72836
Dan Birman <danbirman@gmail.com>
2025-07-03 15:37:04 -0700

    chore: lint

--- tests/test_measurements.py
+++ tests/test_measurements.py
@@ -17,19 +17,13 @@ class TestCalibrationFit(unittest.TestCase):
     def test_linear_interpolation_with_parameters_raises_error(self):
         """Test that linear interpolation fit type raises error with parameters"""
         with self.assertRaises(ValidationError) as context:
-            CalibrationFit(
-                fit_type=FitType.LINEAR_INTERPOLATION,
-                fit_parameters={"slope": 1.0, "intercept": 0.0}
-            )
+            CalibrationFit(fit_type=FitType.LINEAR_INTERPOLATION, fit_parameters={"slope": 1.0, "intercept": 0.0})
         self.assertIn("Fit parameters should not be provided for linear interpolation fit type", str(context.exception))
 
     def test_linear_fit_with_parameters(self):
         """Test that linear fit type works with parameters"""
         parameters = {"slope": 1.5, "intercept": 2.0}
-        fit = CalibrationFit(
-            fit_type=FitType.LINEAR,
-            fit_parameters=parameters
-        )
+        fit = CalibrationFit(fit_type=FitType.LINEAR, fit_parameters=parameters)
         self.assertEqual(fit.fit_type, FitType.LINEAR.value)
         # Compare the fit_parameters as a dict using model_dump()
         self.assertIsNotNone(fit.fit_parameters)
@@ -44,10 +38,7 @@ class TestCalibrationFit(unittest.TestCase):
     def test_other_fit_with_parameters(self):
         """Test that other fit type works with parameters"""
         parameters = {"a": 1.0, "b": 2.0, "c": 3.0}
-        fit = CalibrationFit(
-            fit_type=FitType.OTHER,
-            fit_parameters=parameters
-        )
+        fit = CalibrationFit(fit_type=FitType.OTHER, fit_parameters=parameters)
         self.assertEqual(fit.fit_type, FitType.OTHER.value)
         # Compare the fit_parameters as a dict using model_dump()
         self.assertIsNotNone(fit.fit_parameters)
@@ -62,19 +53,13 @@ class TestCalibrationFit(unittest.TestCase):
     def test_linear_fit_with_none_parameters_raises_error(self):
         """Test that linear fit type raises error with None parameters"""
         with self.assertRaises(ValidationError) as context:
-            CalibrationFit(
-                fit_type=FitType.LINEAR,
-                fit_parameters=None
-            )
+            CalibrationFit(fit_type=FitType.LINEAR, fit_parameters=None)
         self.assertIn("Fit parameters must be provided for linear fit type", str(context.exception))
 
     def test_other_fit_with_none_parameters_raises_error(self):
         """Test that other fit type raises error with None parameters"""
         with self.assertRaises(ValidationError) as context:
-            CalibrationFit(
-                fit_type=FitType.OTHER,
-                fit_parameters=None
-            )
+            CalibrationFit(fit_type=FitType.OTHER, fit_parameters=None)
         self.assertIn("Fit parameters must be provided for other fit type", str(context.exception))
 
 
