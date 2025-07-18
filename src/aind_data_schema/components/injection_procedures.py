390efe40e66015b39e4d2814e098f9154f1a81b0
Dan Birman <danbirman@gmail.com>
2025-07-17 20:25:16 -0700

    chore: typo

--- src/aind_data_schema/components/injection_procedures.py
+++ src/aind_data_schema/components/injection_procedures.py
@@ -7,7 +7,7 @@ from typing import List, Optional
 from aind_data_schema_models.coordinates import AnatomicalRelative
 from aind_data_schema_models.mouse_anatomy import MouseAnatomyModel
 from aind_data_schema_models.pid_names import PIDName
-from aind_data_schema_models.units import CurrentUnit, TimeUnit, VolumeUnit, RateUnit
+from aind_data_schema_models.units import CurrentUnit, TimeUnit, VolumeUnit
 from pydantic import Field, model_validator
 
 from aind_data_schema.base import DataModel, DiscriminatedList
