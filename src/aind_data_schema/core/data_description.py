1a0ae046cffbf68406c01be9dfb0c73c6a1aa4f1
Dan Birman <danbirman@gmail.com>
2026-01-05 17:05:18 -0800

    chore: bump core schema versions properly

--- src/aind_data_schema/core/data_description.py
+++ src/aind_data_schema/core/data_description.py
@@ -37,7 +37,7 @@ class DataDescription(DataCoreModel):
 
     _DESCRIBED_BY_URL = DataCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/data_description.py"
     describedBy: str = Field(default=_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
-    schema_version: SkipValidation[Literal["2.2.2"]] = Field(default="2.2.2")
+    schema_version: SkipValidation[Literal["2.3.0"]] = Field(default="2.3.0")
     license: License = Field(default=License.CC_BY_40, title="License")
 
     subject_id: Optional[str] = Field(
--- src/aind_data_schema/core/instrument.py
+++ src/aind_data_schema/core/instrument.py
@@ -75,7 +75,7 @@ class Instrument(DataCoreModel):
     # metametadata
     _DESCRIBED_BY_URL = DataCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/instrument.py"
     describedBy: str = Field(default=_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
-    schema_version: SkipValidation[Literal["2.1.1"]] = Field(default="2.1.1")
+    schema_version: SkipValidation[Literal["2.2.0"]] = Field(default="2.2.0")
 
     # instrument definition
     location: Optional[str] = Field(default=None, title="Location", description="Location of the instrument")
