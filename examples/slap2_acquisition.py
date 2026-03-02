ee3436196a62907677e1521b63c094692f8f73ae
Dan Birman <danbirman@gmail.com>
2026-03-02 09:41:43 -0800

    refactor: cleaning up acquisition files

--- examples/slap2_acquisition.py
+++ examples/slap2_acquisition.py
@@ -5,11 +5,17 @@ from datetime import datetime, timezone
 from aind_data_schema_models.modalities import Modality
 from aind_data_schema_models.slap2_acquisition_type import Slap2AcquisitionType
 from aind_data_schema_models.units import PowerUnit, SizeUnit, FrequencyUnit, TimeUnit
-from aind_data_schema_models.devices import FilterType
-from aind_data_schema_models.organizations import Organization
+
 from aind_data_schema.components.identifiers import Code
-from aind_data_schema.components.devices import Filter
-from aind_data_schema.components.coordinates import Translation, Scale, CoordinateSystemLibrary, Origin
+
+from aind_data_schema.components.coordinates import (
+    Translation,
+    Scale,
+    CoordinateSystem,
+    Axis,
+    Origin,
+)
+from aind_data_schema_models.coordinates import AxisName, Direction
 from aind_data_schema.core.acquisition import (
     Acquisition,
     DataStream,
@@ -28,38 +34,20 @@ from aind_data_schema.components.configs import (
 from aind_data_schema_models.brain_atlas import CCFv3
 from aind_data_schema_models.stimulus_modality import StimulusModality
 
-origin_ari = CoordinateSystemLibrary.BREGMA_ARI
-origin_ari.name = "Arbitrary Origin ARI"
-origin_ari.axis_unit = SizeUnit.UM
-origin_ari.origin = Origin.ORIGIN
-
-filter_red = Filter(
-    name="Red Emission Filters",
-    serial_number="None",
-    manufacturer=Organization.SEMROCK,
-    filter_type=FilterType.BANDPASS,
-    cut_off_wavelength=725,
-    cut_on_wavelength=585,
-    center_wavelength=655,
-    wavelength_unit=SizeUnit.NM,
-    notes="Includes 585 dichroic mirror and 650/150 filter",
+coordinate_system = CoordinateSystem(
+    name="Arbitrary Origin ARI",
+    origin=Origin.ORIGIN,
+    axis_unit=SizeUnit.UM,
+    axes=[
+        Axis(name=AxisName.AP, direction=Direction.PA),
+        Axis(name=AxisName.ML, direction=Direction.LR),
+        Axis(name=AxisName.SI, direction=Direction.SI),
+    ],
 )
 
-filter_green = Filter(
-    name="Green Emission Filters",
-    serial_number="None",
-    manufacturer=Organization.SEMROCK,
-    filter_type=FilterType.BANDPASS,
-    cut_off_wavelength=580,
-    cut_on_wavelength=500,
-    center_wavelength=540,
-    wavelength_unit=SizeUnit.NM,
-    notes="Includes 585 dichroic mirror and 540/80 filter",
-)
-
-filters_by_name = {
-    "Red Emission Filters": filter_red,
-    "Green Emission Filters": filter_green,
+filter_wavelengths = {
+    "Red": 655,
+    "Green": 540,
 }
 
 instrument_components = [
@@ -113,6 +101,18 @@ channel_intended_measurements = {
 }
 
 stage_offset_from_origin = None
+
+image_to_acquisition_transform = [
+    Scale(
+        scale=[0.25, 0.25],
+    ),
+]
+if stage_offset_from_origin is not None:
+    image_to_acquisition_transform.append(
+        Translation(
+            translation=stage_offset_from_origin,
+        )
+    )
 
 imaging_target_name = "Neuron1"
 
@@ -295,7 +295,7 @@ a = Acquisition(
     instrument_id="SLAP2_1_VCO_1",
     acquisition_type=project_name + ": " + acquisition_type,
     notes="center back of cranial window is coordinate system origin",
-    coordinate_system=origin_ari,
+    coordinate_system=coordinate_system,
     calibrations=[],
     maintenance=[],
     data_streams=[
@@ -320,10 +320,8 @@ a = Acquisition(
                                 DeviceConfig(device_name="Polygonal Scanner"),
                             ],
                             emission_filters=[DeviceConfig(device_name=f"{channel_color} Emission Filters")],
-                            emission_wavelength=filters_by_name[f"{channel_color} Emission Filters"].center_wavelength,
-                            emission_wavelength_unit=filters_by_name[
-                                f"{channel_color} Emission Filters"
-                            ].wavelength_unit,
+                            emission_wavelength=filter_wavelengths[channel_color],
+                            emission_wavelength_unit=SizeUnit.NM,
                         )
                         for path_idx in range(num_paths)
                         for channel_color in active_channels
@@ -331,20 +329,7 @@ a = Acquisition(
                     images=[
                         PlanarImage(
                             channel_name=f"Path {path_idx + 1} {channel_color} channel",
-                            image_to_acquisition_transform=[
-                                Scale(
-                                    scale=[0.25, 0.25],
-                                ),
-                                *(
-                                    [
-                                        Translation(
-                                            translation=stage_offset_from_origin,
-                                        )
-                                    ]
-                                    if stage_offset_from_origin is not None
-                                    else []
-                                ),
-                            ],
+                            image_to_acquisition_transform=image_to_acquisition_transform,
                             dimensions=Scale(
                                 scale=[800, 1280],
                             ),
--- examples/slap2_structure_acquisition.py
+++ examples/slap2_structure_acquisition.py
@@ -5,10 +5,15 @@ from datetime import datetime, timezone
 from aind_data_schema_models.modalities import Modality
 from aind_data_schema_models.slap2_acquisition_type import Slap2AcquisitionType
 from aind_data_schema_models.units import PowerUnit, SizeUnit, FrequencyUnit
-from aind_data_schema_models.devices import FilterType
-from aind_data_schema_models.organizations import Organization
-from aind_data_schema.components.devices import Filter
-from aind_data_schema.components.coordinates import Translation, Scale, CoordinateSystemLibrary, Origin
+
+from aind_data_schema.components.coordinates import (
+    Translation,
+    Scale,
+    CoordinateSystem,
+    Axis,
+    Origin,
+)
+from aind_data_schema_models.coordinates import AxisName, Direction
 from aind_data_schema.core.acquisition import (
     Acquisition,
     DataStream,
@@ -26,38 +31,20 @@ from aind_data_schema.components.configs import (
 )
 from aind_data_schema_models.brain_atlas import CCFv3
 
-origin_ari = CoordinateSystemLibrary.BREGMA_ARI
-origin_ari.name = "Arbitrary Origin ARI"
-origin_ari.axis_unit = SizeUnit.UM
-origin_ari.origin = Origin.ORIGIN
-
-filter_red = Filter(
-    name="Red Emission Filters",
-    serial_number="None",
-    manufacturer=Organization.SEMROCK,
-    filter_type=FilterType.BANDPASS,
-    cut_off_wavelength=725,
-    cut_on_wavelength=585,
-    center_wavelength=655,
-    wavelength_unit=SizeUnit.NM,
-    notes="Includes 585 dichroic mirror and 650/150 filter",
+coordinate_system = CoordinateSystem(
+    name="Arbitrary Origin ARI",
+    origin=Origin.ORIGIN,
+    axis_unit=SizeUnit.UM,
+    axes=[
+        Axis(name=AxisName.AP, direction=Direction.PA),
+        Axis(name=AxisName.ML, direction=Direction.LR),
+        Axis(name=AxisName.SI, direction=Direction.SI),
+    ],
 )
 
-filter_green = Filter(
-    name="Green Emission Filters",
-    serial_number="None",
-    manufacturer=Organization.SEMROCK,
-    filter_type=FilterType.BANDPASS,
-    cut_off_wavelength=580,
-    cut_on_wavelength=500,
-    center_wavelength=540,
-    wavelength_unit=SizeUnit.NM,
-    notes="Includes 585 dichroic mirror and 540/80 filter",
-)
-
-filters_by_name = {
-    "Red Emission Filters": filter_red,
-    "Green Emission Filters": filter_green,
+filter_wavelengths = {
+    "Red": 655,
+    "Green": 540,
 }
 
 harp_start_time = datetime(2022, 7, 12, 7, 00, 00, tzinfo=timezone.utc)
@@ -79,7 +66,19 @@ channel_intended_measurements = {
     "Green": "iGluSnFR4s",
 }
 
-stage_offset_from_origin = None  # or [x, y] in um
+stage_offset_from_origin = None
+
+image_to_acquisition_transform = [
+    Scale(
+        scale=[0.25, 0.25],
+    ),
+]
+if stage_offset_from_origin is not None:
+    image_to_acquisition_transform.append(
+        Translation(
+            translation=stage_offset_from_origin,
+        )
+    )
 
 imaging_target_name = "Neuron1"
 
@@ -153,7 +152,7 @@ a = Acquisition(
     instrument_id="SLAP2_1_VCO_1",
     acquisition_type=project_name + ": " + acquisition_type,
     notes="center back of cranial window is coordinate system origin",
-    coordinate_system=origin_ari,
+    coordinate_system=coordinate_system,
     calibrations=[],
     maintenance=[],
     data_streams=[
@@ -193,10 +192,8 @@ a = Acquisition(
                                 DeviceConfig(device_name="Polygonal Scanner"),
                             ],
                             emission_filters=[DeviceConfig(device_name=f"{channel_color} Emission Filters")],
-                            emission_wavelength=filters_by_name[f"{channel_color} Emission Filters"].center_wavelength,
-                            emission_wavelength_unit=filters_by_name[
-                                f"{channel_color} Emission Filters"
-                            ].wavelength_unit,
+                            emission_wavelength=filter_wavelengths[channel_color],
+                            emission_wavelength_unit=SizeUnit.NM,
                         )
                         for path_idx in range(num_paths)
                         for channel_color in active_channels
@@ -212,20 +209,7 @@ a = Acquisition(
                             / (len(plane_depths[f"Path {path_idx+1}"]) - 1),
                             depth_unit=SizeUnit.UM,
                             channel_name=f"Path {path_idx + 1} {channel_color} channel",
-                            image_to_acquisition_transform=[
-                                Scale(
-                                    scale=[0.25, 0.25],
-                                ),
-                                *(
-                                    [
-                                        Translation(
-                                            translation=stage_offset_from_origin,
-                                        )
-                                    ]
-                                    if stage_offset_from_origin is not None
-                                    else []
-                                ),
-                            ],
+                            image_to_acquisition_transform=image_to_acquisition_transform,
                             dimensions=Scale(
                                 scale=[800, 1280],
                             ),
